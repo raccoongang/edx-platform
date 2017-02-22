4ce1714b748dbc2643d54aa60a720246af1642fa
Maksim Sokolskiy <misokolsky@gmail.com>
2017-02-22 15:23:24 +0200

    feat(gamma): add gamification feature using GAMMA app
    Fix gamification feature import for ficus release.

--- lms/urls.py
+++ lms/urls.py
@@ -20,6 +20,8 @@ from openedx.core.djangoapps.site_configuration import helpers as configuration_
 from openedx.features.enterprise_support.api import enterprise_enabled
 
 from openassessment.fileupload import views_filesystem
+from gamification_metric.views import dashboard as gamification
+import referrals.views
 
 # Uncomment the next two lines to enable the admin:
 if settings.DEBUG or settings.FEATURES.get('ENABLE_DJANGO_ADMIN_SITE'):
@@ -110,6 +112,15 @@ urlpatterns = (
 
     # URLs for API access management
     url(r'^api-admin/', include('openedx.core.djangoapps.api_admin.urls', namespace='api_admin')),
+
+    # Gamification
+    url(r'referral/', include('referrals.urls', app_name="referrals", namespace='referrals')),
+    url(
+        r'^api/get_referral_hash_key/',
+        referrals.views.GetHashKeyView.as_view(),
+        name="get_hash_key"
+    ),
+    url(r'^gamification', gamification, name='gamification'),
 
     url(r'^dashboard/', include('learner_dashboard.urls')),
     url(r'^api/experiments/', include('experiments.urls', namespace='api_experiments')),
