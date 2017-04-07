0aafdc461688fc577b1c80468d8a76ea1183a505
Shico-UA <s_shishka@ukr.net>
2017-04-07 12:03:24 +0000

    Added docstrings, changed response logic  within views file.

--- openedx/core/djangoapps/global_statistics/views.py
+++ openedx/core/djangoapps/global_statistics/views.py
@@ -1,28 +1,36 @@
-from django.views.decorators.csrf import csrf_exempt
+from django.http import Http404, HttpResponse
 from django.utils.decorators import method_decorator
-from django.shortcuts import redirect
+from django.views.decorators.csrf import csrf_exempt
 from django.views.generic import View
+
 from .models import TokenStorage
 
 
 class ReceiveTokenView(View):
     """
-    This view receives a secret token from the remote server and save it to DB.
-    In the future, this will allow us to exchange data with a remote server.
+    Receives a secret token from the remote server and save it to DB.
+    This will allow edx-platform to exchange data with a remote server.
     """
+
     @method_decorator(csrf_exempt)
     def dispatch(self, *args, **kwargs):
         return super(ReceiveTokenView, self).dispatch(*args, **kwargs)
 
     def post(self, request, *args, **kwargs):
+        """
+        Receive generated token from the remote server and save it to DB.
+        
+        `secret_token` is uuid.UUID object converted to string.
+        
+        Return http status code based on success of the token's save operation.
+        """
 
-        # Receive generated token from the remote server and save it to DB.
         try:
             received_data = self.request.POST
             secret_token = str(received_data.get('reverse_token'))
             token_object = TokenStorage.objects.get(pk=1)
             token_object.secret_token = secret_token
             token_object.save()
-            return redirect('/')
+            return HttpResponse(status=200)
         except ValueError:
-            return redirect('/')
+            raise Http404()
