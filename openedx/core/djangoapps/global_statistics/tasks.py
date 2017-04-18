188e87ebf1235386fc2612c9b6b67e1565c515f2
Shico-UA <s_shishka@ukr.net>
2017-04-18 17:06:52 +0000

    Configured edx-platform on aws server.

--- openedx/core/djangoapps/global_statistics/tasks.py
+++ openedx/core/djangoapps/global_statistics/tasks.py
@@ -7,7 +7,7 @@ import requests
 
 from celery.task import task
 
-from lms.envs.devstack_with_worker import PERIODIC_TASK_POST_URL, PLATFORM_LATITUDE, PLATFORM_LONGITUDE, SITE_NAME
+from django.conf import settings
 from xmodule.modulestore.django import modulestore
 from student.models import UserProfile
 from .models import TokenStorage
@@ -21,13 +21,19 @@ def count_data():
     makes a POST request with the data to the appropriate service.
     """
 
+    # OEGS settings
+    oegs_settings = settings.ENV_TOKENS.get('OPEN_EDX_GLOBAL_STATISTICS')
+
     # Get IP address of the platform and convert it to latitude, longitude.
     check_ip_url = 'http://freegeoip.net/json'
     ip_data = requests.get(check_ip_url)
     ip_data_json = json.loads(ip_data.text)
-    if PLATFORM_LATITUDE and PLATFORM_LONGITUDE:
-        latitude = PLATFORM_LATITUDE
-        longitude = PLATFORM_LONGITUDE
+    platform_latitude = oegs_settings.get("PLATFORM_LATITUDE")
+    platform_longitude = oegs_settings.get("PLATFORM_LONGITUDE")
+   
+    if platform_latitude and platform_longitude:
+        latitude = platform_latitude
+        longitude = platform_longitude
     else:
         latitude = ip_data_json['latitude']
         longitude = ip_data_json['longitude']
@@ -42,11 +48,13 @@ def count_data():
     secret_token, created = TokenStorage.objects.get_or_create(pk=1)
 
     # Posting data to receiving server.
-    data_to_send = requests.post(PERIODIC_TASK_POST_URL, data={
+    post_url = oegs_settings.get('OEGS_PERIODIC_TASK_POST_URL')
+    platform_url = "https://" + settings.SITE_NAME   
+    data_to_send = requests.post(post_url, data={
         'courses_amount': courses_amount,
         'students_amount': students_amount,
         'latitude': latitude,
         'longitude': longitude,
-        'platform_url': SITE_NAME,
+        'platform_url': platform_url,
         'secret_token': secret_token.secret_token
         })
