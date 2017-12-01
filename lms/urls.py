2d001f63e8170b2f96eeac1d8df2e819b9221ad8
madeira <ganziy.vova@gmail.com>
2017-12-01 13:41:28 +0000

    add url for course

--- lms/urls.py
+++ lms/urls.py
@@ -649,7 +649,7 @@ urlpatterns += (
 
     # Course experience
     url(
-        r'^courses/{}/course/'.format(
+        r'^courses/{}/courseware/'.format(
             settings.COURSE_ID_PATTERN,
         ),
         include('openedx.features.course_experience.urls'),
