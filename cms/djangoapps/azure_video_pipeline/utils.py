a8487037b3e106e924b1346354ee3791a3e2659b
wowkalucky <wowkalucky@gmail.com>
2017-12-20 10:49:11 +0200

    Add azure configuration validation

--- cms/djangoapps/azure_video_pipeline/utils.py
+++ cms/djangoapps/azure_video_pipeline/utils.py
@@ -1,11 +1,12 @@
 from django.conf import settings
+from django.core.exceptions import ImproperlyConfigured
+from django.utils.translation import ugettext as _
 
 from .media_service import MediaServiceClient
 from .models import AzureOrgProfile
 
 
 def get_azure_config(organization):
-    azure_config = {}
     azure_profile = AzureOrgProfile.objects.filter(organization__short_name=organization).first()
     if azure_profile:
         azure_config = azure_profile.to_dict()
@@ -25,6 +26,13 @@ def get_azure_config(organization):
             'storage_account_name': settings.FEATURES.get('STORAGE_ACCOUNT_NAME'),
             'storage_key': settings.FEATURES.get('STORAGE_KEY')
         }
+    else:
+        raise ImproperlyConfigured(_(
+            'In order to use Azure storage for Video Uploads one of the followings must be configured: '
+            '"Azure organization profile" for certain Organization or global CMS settings. '
+            'All settings are mandatory: "AZURE_CLIENT_ID", "AZURE_CLIENT_SECRET", "AZURE_TENANT", '
+            '"AZURE_REST_API_ENDPOINT", "STORAGE_ACCOUNT_NAME", "STORAGE_KEY".'
+        ))
     return azure_config
 
 
