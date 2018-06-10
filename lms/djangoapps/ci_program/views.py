bd05aa8f5588925ad2c7a576860f69a58531cc0b
raccoondeploy <devops@raccoongang.com>
2018-06-10 23:08:01 +0000

    Replace the view functionality with a program_descriptor and add the login_required decorator

--- lms/djangoapps/ci_program/views.py
+++ lms/djangoapps/ci_program/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.decorators import login_required
 from edxmako.shortcuts import render_to_response
 from ci_program.models import Program
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
@@ -6,31 +7,14 @@ from lms.djangoapps.courseware.courses import get_course
 from openedx.core.lib.courses import course_image_url
 
 
+@login_required
 def show_programs(request, program_name):
     """
     Display the programs page
     """
     program = Program.objects.get(marketing_slug=program_name)
-    
-    courses = []
-    for course_overview in program.get_courses():
-        
-        course_id = course_overview.id
-        block_reference, _, location_as_string = str(course_id).partition(':')
-        course_identifiers = location_as_string.split('+')
-        org, key, run = course_identifiers[0], course_identifiers[1], course_identifiers[2]
-        course_locator = CourseLocator(org, key, run)
-        course_descriptor = get_course(course_locator)
-        
-        courses.append(
-            {
-                "course_key": course_id,
-                "course": course_overview,
-                "course_image": course_image_url(course_descriptor)
-            }
-        )
+    program_descriptor = program.get_program_descriptor(request.user)
 
     context = {}
-    context["program"] = program
-    context["courses"] = courses
+    context["program"] = program_descriptor
     return render_to_response('programs/programs.html', context)
