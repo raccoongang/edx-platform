85d3d558e6f63a90b4082efb3caaa58c496ecbf6
aaron <aaron@codeinstitute.net>
2018-07-01 23:23:09 +0100

    Remove unnecessary import statements

--- lms/djangoapps/ci_program/views.py
+++ lms/djangoapps/ci_program/views.py
@@ -1,10 +1,6 @@
 from django.contrib.auth.decorators import login_required
 from edxmako.shortcuts import render_to_response
 from ci_program.models import Program
-from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
-from opaque_keys.edx.locator import CourseLocator
-from lms.djangoapps.courseware.courses import get_course
-from openedx.core.lib.courses import course_image_url
 
 
 @login_required
