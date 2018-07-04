eba2a53345ea24406593094094669939e55eda6e
ayameeno <ayman.alkurdi@ilearn.ws>
2018-07-04 22:33:57 +0300

    add second URL for signout

--- common/djangoapps/third_party_auth/urls.py
+++ common/djangoapps/third_party_auth/urls.py
@@ -16,6 +16,9 @@ urlpatterns = patterns(
     url(r'^auth/custom_auth_entry', post_to_custom_auth_form, name='tpa_post_to_custom_auth_form'),
     url(r'^auth/saml/metadata.xml', saml_metadata_view),
     url(r'^auth/login/(?P<backend>lti)/$', lti_login_and_complete_view),
-    url(r'^auth/complete/(?P<backend>[^/]+){0}$'.format(extra), signout_for_ws_federation(complete), name='social:complete'),
+    url(r'^auth/complete/(?P<backend>[^/]+){0}$'.format(extra), signout_for_ws_federation(complete), 
+        name='social:complete'),
     url(r'^auth/', include('social_django.urls', namespace='social')),
+    url(r'^auth/complete/(?P<backend>[^/]+){0}$'.format(extra), signout_for_ws_federation(complete),
+        name='social:complete'),
 )
