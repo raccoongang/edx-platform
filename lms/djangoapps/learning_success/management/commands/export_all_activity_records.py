120448922433a839ea580189ed73d16c8739275e
Yoni Lavi <yoni@codeinstitute.net>
2018-07-25 01:43:41 +0100

    first attempt at getting lesson data into our LMS API

--- lms/djangoapps/learning_success/management/commands/export_all_activity_records.py
+++ lms/djangoapps/learning_success/management/commands/export_all_activity_records.py
@@ -1,5 +1,5 @@
+from ci_program.api import get_program_by_program_code
 from django.conf import settings
-from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand, CommandError
 from django.utils import timezone
 from opaque_keys.edx.locator import CourseLocator
@@ -12,28 +12,7 @@ import json
 import requests
 
 
-MODULE_LOCATORS = {
-    'HTML Fundamentals':
-        CourseLocator(u'CodeInstitute', u'HF101', u'2017_T1'),
-    'CSS Fundamentals':
-        CourseLocator(u'CodeInstitute', u'CF101', u'2017_T1'),
-    'JavaScript Fundamentals':
-        CourseLocator(u'CodeInstitute', u'JSF101', u'2017_T1'),
-    'Python Fundamentals':
-        CourseLocator(u'CodeInstitute', u'PF101', u'2017_T1'),
-    'User Centric Frontend Development':
-        CourseLocator(u'codeinstitute', u'FE', u'2017_T3'),
-    'Interactive Frontend Development':
-        CourseLocator(u'CodeInstitute', u'IFD101', u'2017_T3'),
-    'Practical Python':
-        CourseLocator(u'CodeInstitute', u'BD101', u'2017_T1'),
-    'Data Centric Development':
-        CourseLocator(u'CodeInstitute', u'DCP101', u'2017_T3'),
-    'Full Stack Frameworks With Django':
-        CourseLocator(u'CodeInstitute', u'F101', u'2017_T1'),
-    '5 Day Coding Challenge':
-        CourseLocator(u'CodeInstitute', u'5DCC', u'T1_2018'),
-}
+PROGRAM_CODE = 'FS'  # Our Full-Stack program
 
 
 def harvest_course_tree(tree, output_dict, prefix=()):
@@ -45,20 +24,20 @@ def harvest_course_tree(tree, output_dict, prefix=()):
     block_breadcrumbs = prefix + (tree.display_name,)
     block_id = tree.location.block_id
 
+    output_dict[block_id] = block_breadcrumbs
+
     children = tree.get_children()
     for subtree in children:
         harvest_course_tree(subtree, output_dict, prefix=block_breadcrumbs)
-    if not children:  # a leaf block AKA component
-        output_dict[block_id] = block_breadcrumbs
 
 
-def harvest_program(locators):
-    """Harvest the breadcrumbs from all components in the list of locators
+def harvest_program(program):
+    """Harvest the breadcrumbs from all components in the program
 
     Returns a dictionary mapping block IDs to the matching breadcrumbs
     """
     all_blocks = {}
-    for course_locator in locators:
+    for course_locator in program.get_course_locators():
         course = modulestore().get_course(course_locator)
         harvest_course_tree(course, all_blocks)
     return all_blocks
@@ -68,10 +47,6 @@ def format_date(value):
     if isinstance(value, datetime):
         return value.isoformat()
     return value
-
-
-def format_module_units_field(module_name):
-    return module_name.lower().replace(' ', '_') + '_units'
 
 
 def thirty_day_units(student, completion_timestamps):
@@ -85,36 +60,56 @@ def days_into_data(student, completion_timestamps):
     return ','.join(map(str, sorted(days_into_generator)))
 
 
-def completed_units_per_module(breadcrumb_list):
-    return Counter(format_module_units_field(breadcrumbs[0])
-                   for breadcrumbs in breadcrumb_list)
+def format_module_field(module_name, suffix):
+    return module_name.lower().replace(' ', '_') + suffix
 
 
-def all_student_data(all_components):
+def completed_lessons_per_module(breadcrumb_dict):
+    return Counter(format_module_field(breadcrumbs[0], '_lessons')
+                   for breadcrumbs in breadcrumb_dict.keys())
+
+def completed_units_per_module(breadcrumb_dict):
+    return Counter(format_module_field(breadcrumbs[0], '_units')
+                   for breadcrumbs in breadcrumb_dict.keys())
+
+def all_student_data(program):
     """Yield a progress metadata dictionary for each of the students
 
     Input is a pregenerated dictionary mapping block IDs in LMS to breadcrumbs
     """
-    for student in User.objects.all():
+    all_components = harvest_program(program)
+
+    for student in program.enrolled_students.all():
         # Provide default values in cases where student hasn't started
-        latest_unit_complete, latest_unit_breadcrumbs = (None, (u'',)*4)
-        # Dictionary of breadcrumbs to timestamps of completion
+        first_unit_complete = None
+        latest_unit_complete = None
+        latest_unit_breadcrumbs = (u'',) * 4
+        # we care about the lesson level (depth 3) and unit level (depth 4)
+        # Dictionaries of breadcrumbs to timestamps of completion
+        completed_lessons = {}
         completed_units = {}
         for activity in student.studentmodule_set.order_by('modified'):
             block_id = activity.module_state_key.block_id
             breadcrumbs = all_components.get(block_id)
-            if breadcrumbs:
-                # we only care about the unit level (depth 4)
+            if breadcrumbs and len(breadcrumbs) == 3:  # lesson
+                lesson_breadcrumbs = breadcrumbs[:3]
+                # for each lesson learned, store latest timestamp
+                completed_lessons[lesson_breadcrumbs] = activity.modified
+
+            if breadcrumbs and len(breadcrumbs) >= 4:  # unit or inner block
                 unit_breadcrumbs = breadcrumbs[:4]
                 # for each unit learned, store latest timestamp
                 completed_units[unit_breadcrumbs] = activity.modified
-                # remember details of the latest unit overall
+                # remember details of the first and latest unit overall
+                if not first_unit_complete:
+                    first_unit_complete = activity.modified
                 latest_unit_complete = activity.modified
                 latest_unit_breadcrumbs = unit_breadcrumbs
 
         student_dict = {
             'email': student.email,
-            'date_joined': format_date(student.date_joined),
+            'date_joined': format_date(
+                first_unit_complete or student.date_joined),
             'last_login': format_date(student.last_login),
             'latest_unit_completion': format_date(latest_unit_complete),
             'latest_module': latest_unit_breadcrumbs[0].encode('utf-8'),
@@ -125,7 +120,9 @@ def all_student_data(all_components):
             'days_into_data': days_into_data(student, completed_units.values()),
         }
 
-        student_dict.update(completed_units_per_module(completed_units.keys()))
+        student_dict.update(completed_lessons_per_module(completed_lessons))
+        student_dict.update(completed_units_per_module(completed_units))
+
         yield student_dict
 
 
@@ -135,8 +132,8 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         """POST the collected data to the api endpoint from the settings
         """
-        all_components = harvest_program(MODULE_LOCATORS.values())
-        student_data = list(all_student_data(all_components))
+        program = get_program_by_program_code(PROGRAM_CODE)
+        student_data = list(all_student_data(program))
 
         api_endpoint = settings.STRACKR_LMS_API_ENDPOINT
         resp = requests.post(api_endpoint, data=json.dumps(student_data))
