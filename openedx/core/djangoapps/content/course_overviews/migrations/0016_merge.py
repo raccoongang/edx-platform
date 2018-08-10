c91f770c6a77d3027fb15d1de8a951576567c500
Dmitry Gamanenko <dgamanenko@gmail.com>
2018-08-10 16:00:03 +0000

    Fix migrations.

---
+++ openedx/core/djangoapps/content/course_overviews/migrations/0016_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('course_overviews', '0015_auto_20180810_1549'),
+        ('course_overviews', '0015_auto_20180614_1520'),
+    ]
+
+    operations = [
+    ]
