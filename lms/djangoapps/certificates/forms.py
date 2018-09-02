a69b67bf9722343d81e4affe7335744732b9eb4f
Naresh <naresh.makwana@ilearn.ws>
2018-09-02 09:19:14 +0300

    Adding certificate verification page

---
+++ lms/djangoapps/certificates/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from django.conf import settings
+from django.utils.translation import ugettext as _
+
+
+class CertVerificationForm(forms.Form):
+    certificate_uuid = forms.CharField(label=_('Certificate ID'), max_length=255, required=True)
