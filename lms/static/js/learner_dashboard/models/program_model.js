c4e4934512d3834ef4138737a0eee0405f9d6a13
Volodymyr Bergman <wowkalucky@gmail.com>
2018-09-18 10:43:53 +0300

    Add default program image - LMS logo (#584)

--- lms/static/js/learner_dashboard/models/program_model.js
+++ lms/static/js/learner_dashboard/models/program_model.js
@@ -17,9 +17,9 @@
                             authoring_organizations: data.authoring_organizations,
                             detailUrl: data.detail_url,
                             marketingUrl: data.marketing_page_url,
-                            xsmallBannerUrl: data.banner_image['x-small'].url,
-                            smallBannerUrl: data.banner_image.small.url,
-                            mediumBannerUrl: data.banner_image.medium.url,
+                            xsmallBannerUrl: data.banner_image['x-small'] ? data.banner_image['x-small'].url : window.themeLogoUrl,
+                            smallBannerUrl: data.banner_image.small ? data.banner_image.small.url : window.themeLogoUrl,
+                            mediumBannerUrl: data.banner_image.medium ? data.banner_image.medium.url : window.themeLogoUrl,
                             breakpoints: {
                                 max: {
                                     xsmall: '320px',
