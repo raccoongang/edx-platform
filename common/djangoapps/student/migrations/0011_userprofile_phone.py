be93625312bb7d812dba904d0e9fce4c2acb8ec9
Andrey Lykhoman <andrey.likhoman@gmail.com>
2018-09-28 11:40:33 +0300

    delete db_index in phone param

--- common/djangoapps/student/migrations/0011_userprofile_phone.py
+++ common/djangoapps/student/migrations/0011_userprofile_phone.py
@@ -14,6 +14,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='userprofile',
             name='phone',
-            field=models.CharField(db_index=True, max_length=50, blank=True),
+            field=models.CharField(max_length=50, blank=True),
         ),
     ]
