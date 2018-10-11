092ebc4f71e466e1458feafe064701e090665531
Andrey Kryachko <andrey.kryachko@raccoongang.com>
2018-10-11 11:44:47 -0400

    added migrations after 'python manage.py makemigrations --merge'

---
+++ openedx/core/djangoapps/content/course_overviews/migrations/0015_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('course_overviews', '0014_auto_20180412_1149'),
+        ('course_overviews', '0014_auto_20180604_0613'),
+    ]
+
+    operations = [
+    ]
