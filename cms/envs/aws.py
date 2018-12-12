879853c1eafcab5fc4e59c58e7c288ca3ee9722f
Oksana Slusarenko <oksana.slu@gmail.com>
2018-12-12 13:53:24 +0200

    Add settings MAX_ASSET_UPLOAD_FILE_SIZE_IN_MB (#811)

--- cms/envs/aws.py
+++ cms/envs/aws.py
@@ -590,6 +590,10 @@ RETIREMENT_STATES = ENV_TOKENS.get('RETIREMENT_STATES', RETIREMENT_STATES)
 ############## Settings for Course Enrollment Modes ######################
 COURSE_ENROLLMENT_MODES = ENV_TOKENS.get('COURSE_ENROLLMENT_MODES', COURSE_ENROLLMENT_MODES)
 
+#RACCOONGANG
+MAX_ASSET_UPLOAD_FILE_SIZE_IN_MB = FEATURES.get("MAX_ASSET_UPLOAD_FILE_SIZE_IN_MB", 200)
+#RACCOONGANG
+
 ####################### Plugin Settings ##########################
 
 # This is at the bottom because it is going to load more settings after base settings are loaded
