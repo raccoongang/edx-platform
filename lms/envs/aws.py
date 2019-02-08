bec45edc1b4144c57a7591484cd54927aea5246d
Andrey Kryachko <andrey.kryachko@raccoongang.com>
2019-02-08 21:44:30 +0200

    added integration with sentry.io

--- lms/envs/aws.py
+++ lms/envs/aws.py
@@ -917,5 +917,12 @@ ORA2_FILEUPLOAD_CACHE_NAME = ENV_TOKENS.get('ORA2_FILEUPLOAD_CACHE_NAME', 'defau
 ORA2_FILEUPLOAD_ROOT = os.path.join(MEDIA_ROOT, 'submissions_attachments/')
 ORA2_FILEUPLOAD_ROOT = ENV_TOKENS.get('ORA2_FILEUPLOAD_ROOT', ORA2_FILEUPLOAD_ROOT)
 SEARCH_SKIP_ENROLLMENT_START_DATE_FILTERING = FEATURES.get("SEARCH_SKIP_ENROLLMENT_START_DATE_FILTERING", True)
-#### RaccoonGang ####
 
+if AUTH_TOKENS.get('RG_SENTRY_DSN', None):
+    import raven
+    INSTALLED_APPS += ( 'raven.contrib.django.raven_compat', )
+    RAVEN_CONFIG = {
+        'dsn': AUTH_TOKENS.get('RG_SENTRY_DSN'),
+    }
+    raven.fetch_git_sha(REPO_ROOT)
+#### RaccoonGang ####
