b0c459d742329fa8db459ebd30737271d8c9b500
Iana Vasylieva <ianavasylieva@Ianas-MacBook-Pro.local>
2019-02-15 10:21:23 +0200

    Add username and email fields to new grade API

--- lms/djangoapps/api_extensions/serializers.py
+++ lms/djangoapps/api_extensions/serializers.py
@@ -1,15 +1,25 @@
 from rest_framework import serializers
 from lms.djangoapps.grades.models import PersistentCourseGrade
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class PersistentCourseGradeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = PersistentCourseGrade
-        fields = ('course_id', 'user_id', 'percent_grade', 'letter_grade', 'passed_timestamp', 'course_title')
+        fields = ('course_id', 'user_id', 'percent_grade', 'letter_grade', 'passed_timestamp', 'course_title', 'username', 'email')
 
     course_title = serializers.SerializerMethodField()
+    username = serializers.SerializerMethodField()
+    email = serializers.SerializerMethodField()
 
     def get_course_title(self, obj):
         return CourseOverview.objects.get(id=obj.course_id).display_name
+
+    def get_username(self, obj):
+        return User.objects.get(id=obj.user_id).username
+
+    def get_email(self, obj):
+        return User.objects.get(id=obj.user_id).email
