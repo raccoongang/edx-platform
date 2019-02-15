146f7005b0f05ac35f144d7978d2bd3a652d2aff
Iana <33125830+noisysky@users.noreply.github.com>
2019-02-15 11:43:36 +0200

    [SDN] Add username and email fields to new grade API (#956)

--- lms/djangoapps/api_extensions/serializers.py
+++ lms/djangoapps/api_extensions/serializers.py
@@ -1,15 +1,40 @@
 from rest_framework import serializers
 from lms.djangoapps.grades.models import PersistentCourseGrade
 from openedx.core.djangoapps.content.course_overviews.models import CourseOverview
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class PersistentCourseGradeSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = PersistentCourseGrade
-        fields = ('course_id', 'user_id', 'percent_grade', 'letter_grade', 'passed_timestamp', 'course_title')
+        fields = ('course_id', 'user_id', 'percent_grade', 'letter_grade', 'passed_timestamp', 'course_title', 'username', 'email')
 
     course_title = serializers.SerializerMethodField()
+    username = serializers.SerializerMethodField()
+    email = serializers.SerializerMethodField()
 
     def get_course_title(self, obj):
-        return CourseOverview.objects.get(id=obj.course_id).display_name
+        try:
+            course = CourseOverview.objects.get(id=obj.course_id)
+        except CourseOverview.DoesNotExist:
+            return None
+        else:
+            return course.display_name
+
+    def get_username(self, obj):
+        try:
+            user = User.objects.get(id=obj.user_id)
+        except User.DoesNotExist:
+            return None
+        else:
+            return user.username
+
+    def get_email(self, obj):
+        try:
+            user = User.objects.get(id=obj.user_id)
+        except User.DoesNotExist:
+            return None
+        else:
+            return user.email
