1996c2af6a12b0787e0fd80ea77f9f4566dcdf14
Arsen Turchanikov <arsen.tur@gmail.com>
2019-03-14 23:53:25 +0200

    Refactor code

--- openedx/core/djangoapps/bookmarks/api.py
+++ openedx/core/djangoapps/bookmarks/api.py
@@ -166,16 +166,14 @@ def delete_bookmarks(usage_key):
     units_keys = []
 
     if usage_key.block_type == u'vertical':
-        query = {'usage_key': usage_key}
+        units_keys.appent(usage_key)
     else:
         # NOTE(arsentur) Get all children for deleted block
         section = modulestore().get_item(usage_key)
         for sub_section in section.get_children():
             units_keys += [unit.location for unit in sub_section.get_children()]
 
-        query = {'usage_key__in': units_keys}
-
-    bookmarks = Bookmark.objects.filter(**query)
+    bookmarks = Bookmark.objects.filter(usage_key__in=units_keys)
     [_track_event('edx.bookmark.removed', b) for b in bookmarks]
     bookmarks.delete()
 
