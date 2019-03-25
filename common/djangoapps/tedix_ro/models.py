d2911619164e31183648cf1247a402701a7787c4
Volodymyr Seleznov <vladimir.seleznev@raccoongang.com>
2019-03-25 13:50:28 +0200

    [TEDIX] login validation
    - https://youtrack.raccoongang.com/issue/T-21

--- common/djangoapps/tedix_ro/models.py
+++ common/djangoapps/tedix_ro/models.py
@@ -1,7 +1,13 @@
+from django.conf import settings
+from django.contrib.auth import login
 from django.contrib.auth.models import User
+from django.contrib.auth.signals import user_logged_in
 from django.core.validators import RegexValidator
 from django.db import models
+from django.dispatch import receiver
 from django.utils.translation import ugettext_lazy as _
+
+from student.models import AUDIT_LOG
 
 phone_validator = RegexValidator(regex=r'^\d{10,15}$', message='Phone length should to be from 10 to 15')
 
@@ -16,3 +22,19 @@ class StudentParent(models.Model):
 
     def __unicode__(self):
         return unicode(self.user)
+
+
+@receiver(user_logged_in)
+def student_parent_logged_in(sender, request, user, **kwargs):  # pylint: disable=unused-argument
+    """
+    Relogin as student when parent logins successfully
+    """
+    try:
+        parent_profile = user.parent_profile
+        AUDIT_LOG.info(u'Parent Login success - {0} ({1})'.format(user.username, user.email))
+        student = parent_profile.students.first()
+        if student is not None and student.is_active:
+            login(request, student, backend=settings.AUTHENTICATION_BACKENDS[0])
+            AUDIT_LOG.info(u'Relogin as parent student - {0} ({1})'.format(student.username, student.email))
+    except StudentParent.DoesNotExist:
+        pass
