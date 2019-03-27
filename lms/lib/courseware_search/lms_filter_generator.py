43ee8c3f0d6755a3df69cc3f609fcf9601755451
Oksana Slusarenko <oksana.slu@gmail.com>
2019-03-27 09:26:25 +0200

    Course with Visibility In Catalog none is still visible (#789) (#1067) (#1069)

--- lms/lib/courseware_search/lms_filter_generator.py
+++ lms/lib/courseware_search/lms_filter_generator.py
@@ -2,6 +2,7 @@
 This file contains implementation override of SearchFilterGenerator which will allow
     * Filter by all courses in which the user is enrolled in
 """
+from xmodule.course_module import CATALOG_VISIBILITY_NONE
 
 from student.models import CourseEnrollment
 from search.filter_generator import SearchFilterGenerator
@@ -52,4 +53,5 @@ class LmsSearchFilterGenerator(SearchFilterGenerator):
             if org_filter_out_set:
                 exclude_dictionary['org'] = list(org_filter_out_set)
 
+        exclude_dictionary['catalog_visibility'] = [CATALOG_VISIBILITY_NONE]
         return exclude_dictionary
