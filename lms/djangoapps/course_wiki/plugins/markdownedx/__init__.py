e42a31a8b99158d6ea27c36f13d37f6145aca69d
Yura [flying_pi] Braiko <fp.flyingpi@gmail.com>
2019-04-17 10:03:53 +0300

    remove local deploy artifact

--- lms/djangoapps/course_wiki/plugins/markdownedx/__init__.py
+++ lms/djangoapps/course_wiki/plugins/markdownedx/__init__.py
@@ -1,3 +1,2 @@
 # Make sure wiki_plugin.py gets run.
-#TODO (flying_pi) commented for run lms
-#from course_wiki.plugins.markdownedx.wiki_plugin import ExtendMarkdownPlugin
+from course_wiki.plugins.markdownedx.wiki_plugin import ExtendMarkdownPlugin
