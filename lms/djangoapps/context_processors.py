b7175ff19e9da1d54e8318acdc1180c371ee4b1b
Michael Khaimov <khaimovmr@gmail.com>
2019-05-21 12:43:48 +0300

    4a-32: edx-platform part (#1209)

--- lms/djangoapps/context_processors.py
+++ lms/djangoapps/context_processors.py
@@ -1,10 +1,17 @@
 """
 Django Template Context Processor for LMS Online Contextual Help
 """
+import logging
+import urllib3
+import xmltodict
 import ConfigParser
 from django.conf import settings
 
 from util.help_context_processor import common_doc_url
+from urllib3.exceptions import MaxRetryError
+
+
+logger = logging.getLogger(__file__)
 
 
 # Open and parse the configuration file when the module is initialized
@@ -26,3 +33,37 @@ def doc_url(request=None):  # pylint: disable=unused-argument
             May be used in the future for determining the language of choice.
     """
     return common_doc_url(request, CONFIG)
+
+
+def xml_header_and_footer(request):
+    """
+    Renders a custom html header and footer, received from the Microsoft's side.
+
+    :request: HttpRequest object instance that was passed from the view
+    :returns: dictionary of the four filters ('xml_css_includes', 'xml_js_includes', `xml_header`, `xml_footer`)
+              added by this context processor
+    """
+    try:
+        connection = urllib3.connection_from_url('https://uhf.microsoft.com')
+        response = connection.request(
+            'get',
+            settings.FOURAFRIKA_XML_URL,
+        )
+        raw_xml_dict = xmltodict.parse(response.data)
+        xml_dict = raw_xml_dict['shell']
+    except MaxRetryError:
+        xml_dict = {
+            'cssIncludes': '',
+            'javascriptIncludes': '',
+            'headerHtml': '',
+            'footerHtml': '',
+        }
+        logger.exception('Connect to the header/footer xml resource failed')
+
+    filters = {
+        'xml_css_includes': xml_dict.get('cssIncludes'),
+        'xml_js_includes': xml_dict.get('javascriptIncludes'),
+        'xml_header': xml_dict.get('headerHtml'),
+        'xml_footer': xml_dict.get('footerHtml'),
+    }
+    return filters
