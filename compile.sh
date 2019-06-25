7a609875b9f92624e917d7eae8c5c50a97e7c3d3
Alexei Kornienko <alexei.kornienko@raccoongang.com>
2019-06-25 18:59:14 +0300

    Define proper env variables for each subsystem

--- compile.sh
+++ compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 . .node/bin/activate && \
     paver install_node_prereqs && \
-    paver update_assets cms --settings aws && \
-    paver update_assets lms --settings aws && \
+    NO_PREREQ_INSTALL=True STATIC_ROOT=/staticfiles/studio paver update_assets cms --settings aws && \
+    NO_PREREQ_INSTALL=True paver update_assets lms --settings aws && \
     echo "Done"
