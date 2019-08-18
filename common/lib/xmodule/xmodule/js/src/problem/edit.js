4831a21d5c757ca313fbbf82432ab5c1d95c3908
sendr <sendr84@gmail.com>
2019-08-18 21:36:47 +0300

    Capa Visual Editor by Raccoon Gang (PR #184)

--- common/lib/xmodule/xmodule/js/src/problem/edit.js
+++ common/lib/xmodule/xmodule/js/src/problem/edit.js
@@ -214,10 +214,15 @@
             this.element.off('click', '.format-buttons button', this.onToolbarButton);
             this.element.off('click', '.cheatsheet-toggle', this.toggleCheatsheet);
             if (this.current_editor === this.markdown_editor) {
+                var markdown = this.markdown_editor.getValue();
+                // If the visual editor has been used to make edits, get the latest markdown from it:
+                if (window.LXCData && window.LXCData.editorHasChanges) {
+                    markdown = window.LXCData.markdown;
+                }
                 return {
-                    data: MarkdownEditingDescriptor.markdownToXml(this.markdown_editor.getValue()),
+                    data: MarkdownEditingDescriptor.markdownToXml(markdown),
                     metadata: {
-                        markdown: this.markdown_editor.getValue()
+                        markdown: markdown
                     }
                 };
             } else {
