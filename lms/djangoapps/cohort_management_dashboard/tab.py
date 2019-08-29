9cc25045d5fa662140f32d9fa4e6921ec9ec544f
yurabraiko <yurabraiko@gmail.com>
2019-08-29 19:30:02 +0300

    moved static files of the cohort dashboard to the common static dir

--- lms/djangoapps/cohort_management_dashboard/tab.py
+++ lms/djangoapps/cohort_management_dashboard/tab.py
@@ -1,11 +1,11 @@
-from django.utils.translation import ugettext_noop
+from django.utils.translation import ugettext as _
 
 from courseware.tabs import EnrolledTab
 
 class CohortManagementDashboardTab(EnrolledTab):
 
     type = "cohort_management_dashboard"
-    title = 'Cohort Management Dashboard'
+    title = _('Cohort Management Dashboard')
     view_name = "cohort_management_dashboard:dashboard"
     is_dynamic = True
 
