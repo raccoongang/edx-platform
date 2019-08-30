2c5f5f209d3e5d8e4f64e4be03c06ab3d32072b2
Max K <cmltaWt0@gmail.com>
2019-08-30 11:49:19 +0300

    Add gamification page
    TODO:
    - rewrite this as a separate app
    - think about using LTI/IFrame to render gamification page

--- lms/djangoapps/learner_dashboard/urls.py
+++ lms/djangoapps/learner_dashboard/urls.py
@@ -1,5 +1,6 @@
 """Learner dashboard URL routing configuration"""
 from django.conf.urls import url
+from django.conf import settings
 
 from lms.djangoapps.learner_dashboard import programs, views
 
@@ -10,3 +11,9 @@ urlpatterns = [
     url(r'^programs/(?P<program_uuid>[0-9a-f-]+)/details_fragment/$', programs.ProgramDetailsFragmentView.as_view(),
         name='program_details_fragment_view'),
 ]
+
+
+if settings.FEATURES.get('RG_GAMIFICATION', {}).get('ENABLED'):
+    urlpatterns += [
+        url(r'^gamification/$', views.gamification_dashboard, name='gamification'),
+    ]
