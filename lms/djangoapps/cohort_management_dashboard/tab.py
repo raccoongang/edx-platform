a0353951a74087f480bb6dd1c8661d1923b75ae0
Golub Sergey <1.golub.sergey.1@gmail.com>
2019-09-10 13:07:50 +0300

    hides cohort management dashboard for learner roles

--- lms/djangoapps/cohort_management_dashboard/tab.py
+++ lms/djangoapps/cohort_management_dashboard/tab.py
@@ -1,5 +1,6 @@
 from django.utils.translation import ugettext as _
 
+from courseware.access import has_access
 from courseware.tabs import EnrolledTab
 
 class CohortManagementDashboardTab(EnrolledTab):
@@ -11,5 +12,10 @@ class CohortManagementDashboardTab(EnrolledTab):
 
     @classmethod
     def is_enabled(cls, course, user=None):
-        return super(CohortManagementDashboardTab, cls).is_enabled(course, user)
+        if (
+            has_access(user, 'staff', course, course.id) or has_access(user, 'instructor', course, course.id)
+            or user.is_staff or user.is_superuser
+        ):
+            return super(CohortManagementDashboardTab, cls).is_enabled(course, user)
+        return False
 
