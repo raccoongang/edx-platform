38f9c629e683876885810ab6e5d8867987ecb16c
Oksana Slusarenko <oksana.slu@gmail.com>
2019-10-24 15:08:16 +0300

    Check student activity on courseware (#1609) (#1612)

--- lms/djangoapps/courseware/admin.py
+++ lms/djangoapps/courseware/admin.py
@@ -2,12 +2,18 @@
 django admin pages for courseware model
 '''
 
+from config_models.admin import ConfigurationModelAdmin
 from ratelimitbackend import admin
 
-from courseware.models import OfflineComputedGrade, OfflineComputedGradeLog, StudentModule
+from courseware.models import (
+    OfflineComputedGrade, OfflineComputedGradeLog,
+    StudentModule, UserCheckActivityConfig
+)
 
 admin.site.register(StudentModule)
 
 admin.site.register(OfflineComputedGrade)
 
 admin.site.register(OfflineComputedGradeLog)
+
+admin.site.register(UserCheckActivityConfig, ConfigurationModelAdmin)
