0144c27c87eae86e42a32999d69d27993fddc971
Alexander Marenich <sendr84@gmail.com>
2019-12-02 20:36:30 +0200

    HERA-15 [dev] Onboarding pages
    fixed onboarding pages when last page

--- lms/djangoapps/hera/views.py
+++ lms/djangoapps/hera/views.py
@@ -5,6 +5,7 @@ View which retrieve hera onboarding pages and handle user onboarding states.
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponseForbidden, JsonResponse
 from django.views.generic import View
+from django.urls import reverse
 from django.utils.decorators import method_decorator
 
 from edxmako.shortcuts import render_to_response
@@ -24,10 +25,13 @@ class OnboardingPagesView(View):
         """
         onboarding = Onboarding.objects.first()
         user_onboarding, _ = UserOnboarding.objects.select_related('onboarding').get_or_create(user=request.user, onboarding=onboarding)
+        # redirect_url = reverse('hera:selection_page', args={'course_id': kwargs['course_id']})
         context = {
             'pages': user_onboarding.get_pages(),
             'current_page': user_onboarding.get_current_page(),
-            'is_passed': user_onboarding.is_passed()
+            'is_passed': user_onboarding.is_passed(),
+            'course_id': kwargs['course_id'],
+            # 'redirect_url': redirect_url
         }
         return render_to_response("hera/onboarding.html", context)
 
@@ -38,11 +42,10 @@ class OnboardingPagesView(View):
         page = request.POST.get('page')
         if request.is_ajax() and page:
             user_onboarding = UserOnboarding.update(self.request.user, page)
-
             return JsonResponse({
                 'success': True,
                 'next_page': user_onboarding.get_next_page(page),
                 'current_page': page,
-                'is_passed': user_onboarding.is_passed()
+                'is_passed': user_onboarding.is_passed(),
             })
         return HttpResponseForbidden()
