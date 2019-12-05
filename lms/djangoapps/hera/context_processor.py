e78896ab82d8fcddfac26b919d7169db83cf7bd2
Alexander Marenich <sendr84@gmail.com>
2019-12-05 22:14:38 +0200

    corrections

--- lms/djangoapps/hera/context_processor.py
+++ lms/djangoapps/hera/context_processor.py
@@ -5,10 +5,10 @@ def user_onboarding(request):
     if request.user.is_authenticated():
         user_onboarding = UserOnboarding.objects.filter(user=request.user).first()
         onboarding_data = {
-            'onboarding_pages': user_onboarding.get_pages(),
             'is_onboarding_passed': False,
         }
         if user_onboarding and user_onboarding.is_passed():
             onboarding_data['is_onboarding_passed'] = True
+            onboarding_data['onboarding_pages'] = user_onboarding.get_pages()
         return onboarding_data
     return {}
--- lms/djangoapps/hera/middleware.py
+++ lms/djangoapps/hera/middleware.py
@@ -6,12 +6,22 @@ class OnboardingMiddleware(object):
     """
     Redirect users to the Onboarding Page if they haven't passed it yet.
     """
+    ALLOWED_URLS = [
+        'onboarding',
+        'logout',
+        'admin',
+    ]
+
+    def is_allowed(self, current_path):
+        for path in self.ALLOWED_URLS:
+            if current_path.replace('/', '').startswith(path):
+                return True
+        return False
 
     def process_request(self, request):
-        allowed_urls = [reverse('hera:onboarding'), reverse('logout')]
-        is_path_allowed = request.path in allowed_urls
+        is_path_allowed = self.is_allowed(request.path)
         is_ajax = request.META.get("HTTP_X_REQUESTED_WITH") == 'XMLHttpRequest'
-        if request.user.is_authenticated() and not is_ajax and not is_path_allowed:
+        if request.user.is_authenticated() and not request.user.is_staff and not is_ajax and not is_path_allowed:
             user_onboarding = UserOnboarding.objects.filter(user=request.user).first()
             if not user_onboarding or not user_onboarding.is_passed():
                 return HttpResponseRedirect(reverse('hera:onboarding'))
