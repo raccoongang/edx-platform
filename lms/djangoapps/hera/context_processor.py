74e207139143f6640d8780a3308cfcfd6f6829ee
Alexander Marenich <sendr84@gmail.com>
2019-12-05 15:01:41 +0200

    HERA-99 Onboarding pages displaying in modal. - Added hera.context_processor - redirect from the Onboarding Page if it's already passed.

---
+++ lms/djangoapps/hera/context_processor.py
@@ -0,0 +1,14 @@
+from .models import UserOnboarding
+
+
+def user_onboarding(request):
+    if request.user.is_authenticated():
+        user_onboarding = UserOnboarding.objects.filter(user=request.user).first()
+        onboarding_data = {
+            'onboarding_pages': user_onboarding.get_pages(),
+            'is_onboarding_passed': False,
+        }
+        if user_onboarding and user_onboarding.is_passed():
+            onboarding_data['is_onboarding_passed'] = True
+        return onboarding_data
+    return {}
