4bd3644032f2a8f0c3a2aae40655b377bf4d07c8
o.bugaenko <oleksandr.buhaienko@raccoongang.com>
2019-12-20 12:33:18 +0200

    [HERA-115] add accordion styles to the nav panel

--- cms/static/js/hera/containers/Questions.jsx
+++ cms/static/js/hera/containers/Questions.jsx
@@ -15,27 +15,26 @@ export default class Questions extends React.Component{
         return (
             <div>
                 <div className="nav-panel-accrd">
-                    <a href="#" onClick={() => null} className="nav-panel-accrd__link">
-                        Questions
-                    </a>
-                </div>
-                <ul className="nav-panel-questions-list nav-panel-accrd__content">
-                    {this.props.questions.map((question, index) => {
-                        return (
-                            <li className="nav-panel-questions-list__item" key={index}>
-                                <SwitchComponent isActive={activeQuestionIndex == index} switchComponent={this.props.switchComponent} title={`Question${index+1}`}/>
-                                <span className="remove-item">
+                    <input type="checkbox" id="chck1" />
+                    <label className="tab-label nav-panel-accrd__link" htmlFor="chck1">Questions</label>
+                    <ul className="nav-panel-questions-list nav-panel-accrd__content">
+                        {this.props.questions.map((question, index) => {
+                            return (
+                                <li className="nav-panel-questions-list__item" key={index}>
+                                    <SwitchComponent isActive={activeQuestionIndex == index} switchComponent={this.props.switchComponent} title={`Question${index+1}`}/>
+                                    <span className="remove-item">
                                     <i className="fa fa-trash-o" aria-hidden="true"></i>
                                 </span>
-                            </li>
-                        )
-                    })}
-                    <li className="nav-panel-questions-list__item">
-                        <button className="add-btn">
-                           + add question
-                        </button>
-                    </li>
-                </ul>
+                                </li>
+                            )
+                        })}
+                        <li className="nav-panel-questions-list__item">
+                            <button className="add-btn">
+                                + add question
+                            </button>
+                        </li>
+                    </ul>
+                </div>
             </div>
         )
     }
