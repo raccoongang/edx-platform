e2db168bf363c2a3319f542cbcee0567777efd7c
Sagirov Eugeniy <sagirov19@gmail.com>
2019-12-20 17:47:06 +0200

    [Development][LMS part] - XBlock "Question" - Base frontend functionality.

--- hera_xblocks/question/question/question.py
+++ hera_xblocks/question/question/question.py
@@ -1,22 +1,24 @@
 """TO-DO: Write a description of what this XBlock is."""
-
 import pkg_resources
 from web_fragments.fragment import Fragment
 from xblock.core import XBlock
-from xblock.fields import JSONField
+from xblock.fields import JSONField, Scope, List, Integer
 from xblockutils.resources import ResourceLoader
 from xblockutils.studio_editable import StudioEditableXBlockMixin
 
-
 loader = ResourceLoader(__name__)
+
 
 class QuestionXBlock(StudioEditableXBlockMixin, XBlock):
     """
     TO-DO: document what your XBlock does.
     """
     editable_fields = ('data',)
+    has_score = True
 
-    data = JSONField()
+    data = JSONField(default={})
+    user_confidence = Integer(scope=Scope.user_state)
+    user_answer = List(scope=Scope.user_state)
 
     @property
     def img_url(self):
@@ -31,12 +33,30 @@ class QuestionXBlock(StudioEditableXBlockMixin, XBlock):
         return self.data.get("description")
 
     @property
-    def options(self):
-        return self.data.get("options")
+    def question(self):
+        return self.data.get("question")
 
     @property
-    def user_confidence(self):
-        return self.data.get("userConfidence")
+    def options(self):
+        return self.data.get("question", {}).get("options")
+
+    @property
+    def answer(self):
+        return self.question.get("answer")
+
+    @property
+    def preciseness(self):
+        preciseness = self.question.get("preciseness")
+        preciseness_values = preciseness.split('%')
+        try:
+            preciseness_value = float(preciseness_values[0]) if preciseness_values else 0
+        except ValueError:
+            preciseness_value = 0
+
+        if preciseness.rfind('%') > -1:
+            return preciseness_value * self.answer / 100
+        else:
+            return preciseness_value
 
     @property
     def confidence_text(self):
@@ -69,11 +89,18 @@ class QuestionXBlock(StudioEditableXBlockMixin, XBlock):
 
     def get_context(self):
         return {
+            "user_answer": self.user_answer,
+            "question": self.question,
             "img_url": self.img_url,
             "iframe_url": self.iframe_url,
             "description": self.description,
             "confidence_text": self.confidence_text,
-            "options": self.options
+            "correct_answer_text": self.correct_answer_text,
+            "incorrect_answer_text": self.incorrect_answer_text,
+            "options": self.options,
+            "rephrase": self.rephrase,
+            "break_down": self.break_down,
+            "teach_me": self.teach_me
         }
 
     def student_view(self, context=None):
@@ -89,15 +116,48 @@ class QuestionXBlock(StudioEditableXBlockMixin, XBlock):
         frag.add_css(self.resource_string("static/css/question.css"))
         frag.add_javascript_url("https://cdn.jsdelivr.net/gh/vast-engineering/jquery-popup-overlay@2/jquery.popupoverlay.min.js")
         frag.add_javascript(self.resource_string("static/js/src/question.js"))
-        frag.initialize_js('QuestionXBlock')
+        frag.initialize_js('QuestionXBlock', json_args=self.get_context())
         return frag
-    
+
     @XBlock.json_handler
     def get_data(self, somedata, sufix=''):
         return self.data
 
     @XBlock.json_handler
-    def submit(self, answer, suffix=''):
-        correct_answers = [ option["title"] for option in self.options if option["correct"]==True ]
-        print(answer, correct_answers)
-        return True if answer in correct_answers else False
+    def submit(self, data, suffix=''):
+        user_answers = []
+        correct = False
+        answers = data.get("answers")
+        try:
+            self.user_confidence = int(data.get("confidence"))
+        except ValueError:
+            self.user_confidence = None
+
+        if self.question.get('type') == "number":
+            for answer in answers:
+                try:
+                    user_answer = float(answer.get('value'))
+                    user_answers.append(user_answer)
+                    correct_answer = self.answer
+                    if correct_answer - self.preciseness <= user_answer <= correct_answer + self.preciseness:
+                        correct = True
+                except ValueError:
+                    user_answer = None
+
+        elif self.question.get('type') == "text":
+            for answer in answers:
+                user_answers.append(answer.get('value'))
+                if answer.get('value') == self.answer:
+                    correct = True
+
+        elif self.question.get('type') in ["select", "radio", "checkbox"]:
+            correct_answers = [ option["title"] for option in self.options if option["correct"] is True ]
+            for answer in answers:
+                user_answers.append(answer.get('value'))
+            if set(user_answers) == set(correct_answers):
+                correct = True
+
+        grade_value = 1 if correct else 0
+        self.runtime.publish(self, 'grade', {'value': grade_value, 'max_value': 1})
+        self.user_answer = user_answers
+        return correct
--- hera_xblocks/question/question/static/js/src/question.js
+++ hera_xblocks/question/question/static/js/src/question.js
@@ -1,26 +1,82 @@
 /* Javascript for QuestionXBlock. */
-function QuestionXBlock(runtime, element) {
-
+function QuestionXBlock(runtime, element, init_args) {
     var submithHandlerUrl = runtime.handlerUrl(element, 'submit');
 
     $(function ($) {
-        // Initialize the plugin
-        $('#iframe').popup();
+        var $confidenceInfo = $(".confidence-text.info", element);
+        var $confidenceInput = $(".confidence-input", element);
+        var $scaffolds = $(".scaffolds", element);
+        var $blockScaffold = $(".scaffold-info", element);
+        var $close = $(".scaffold-img-close-btn", element);
+        var $skip = $('.skip', element);
+        var $questionForm = $(".question-form", element);
+        var $questionImage = $(".question__image", element);
+        var $questionContent = $(".question__content", element);
+        var $submit = $('.submit', element);
 
-        // Set default `pagecontainer` for all popups (optional, but recommended for screen readers and iOS*)
-        $.fn.popup.defaults.pagecontainer = '.author-block__content'
+        $('[name=answer]', element).on("change blur keyup", function() {
+            $confidenceInfo.removeClass("hidden");
+            $confidenceInput.removeClass("hidden is-not-valid");
+            $submit.removeAttr("disabled");
+            $confidenceInfo.text(init_args.confidence_text)
+        });
+
+        $submit.bind('click', function () {
+            if ($confidenceInput.val() && $confidenceInput.is(':valid')){
+                var answers = $questionForm.serializeArray();
+                var confidence = $(".confidence-input", element).val();
+                $.post(submithHandlerUrl, JSON.stringify({"answers":answers, "confidence":confidence})).done(function (response) {
+                    if (response === true) {
+                        $skip.addClass("hidden");
+                        $scaffolds.addClass("hidden");
+                        $confidenceInfo.text(init_args.correct_answer_text);
+                    }
+                    else {
+                        $skip.removeClass("hidden");
+                        $scaffolds.removeClass("hidden");
+                        $confidenceInfo.text(init_args.incorrect_answer_text);
+                    }
+                    $confidenceInput.addClass("hidden");
+                });
+            } else {
+                $confidenceInput.addClass("is-not-valid");
+            };
+        });
+
+        $('.scaffold', element).bind('click', function (event) {
+            scaffoldData = $(event.target).data();
+
+            $blockScaffold.removeClass("hidden");
+            // TODO: need to substitute img and description for mascot
+            // $(".scaffold-img", element).attr('src', scaffoldData.img_url);
+            // $(".scaffold-description", element).html("Some text");
+
+            $questionForm.addClass("hidden");
+            $questionImage.attr('src', scaffoldData.img_url);
+            $questionContent.html(scaffoldData.content);
+            $close.removeClass("hidden");
+        });
+
+        $('.try_again', element).bind('click', function (event) {
+            $blockScaffold.addClass("hidden");
+
+            $questionForm.removeClass("hidden");
+            $questionContent.html(init_args.description);
+        });
+
+        $close.bind('click', function (event) {
+            $close.addClass("hidden");
+            $questionImage.attr('src', init_args.img_url);
+        });
 
 
-        $(element).find('#submit').bind('click', function () {
-            var answer = $('#question').serializeArray();
-            debugger;
-            $.post(submithHandlerUrl, JSON.stringify(answer)).done(function (response) {
-                if (response === true) {
-                    alert('OK');
-                }
-                else {
-                    alert('Not OK');
-                }
+        $(document).ready(function() {
+            $('.show-simulation', element).click(function() {
+               $('.simulation-overlay', element).fadeIn(300);
+            });
+         
+            $('.simulation-close-btn', element).click(function() {
+               $('.simulation-overlay', element).fadeOut(300);
             });
         });
     });
