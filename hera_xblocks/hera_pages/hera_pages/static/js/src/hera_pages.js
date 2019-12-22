27048c879c4dbede50dbb0ed2177a16b177190c3
Ruslan Skira <ruslan.skira@raccoongang.com>
2019-12-22 15:23:17 +0200

    added buttons next previous to html slider

--- hera_xblocks/hera_pages/hera_pages/static/js/src/hera_pages.js
+++ hera_xblocks/hera_pages/hera_pages/static/js/src/hera_pages.js
@@ -1,5 +1,7 @@
 /* Javascript for HeraPagesXBlock. */
 function HeraPagesXBlock(runtime, element) {
+    var isFormValid;
+    
     $('.slidebar-wrapper').slick({
         slidesToShow: 1,
         slidesToScroll: 1,
@@ -8,6 +10,32 @@ function HeraPagesXBlock(runtime, element) {
         adaptiveHeight: true
         // asNavFor: '.slider-nav'
       });
+
+    let shouldShowMessage = true;
+      
+    $(".button-prev").click((e) => {
+        $(".slick-prev").trigger("click")
+    });
+
+    $(".button-next").click((e) => {
+        if(shouldShowMessage && !isFormValid()) {
+            shouldShowMessage = false;
+            alert('Fill up al the fields');
+        } else {
+            $(".slick-next").trigger("click")
+        }
+    })  
+    
+    isFormValid = function () {
+        const $inputElements = $('.slick-active .column-input');
+        let result = true;
+        if ($inputElements.length) {
+            $inputElements.each((i, el) => {
+                if(!$(el).val()) {
+                    result = false;
+                }
+            });
+        }
+        return result;
+    }
 }
-
-
