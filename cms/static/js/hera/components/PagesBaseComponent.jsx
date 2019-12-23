da61c722968300dc116e8e22ae771e4e5edb22b1
o.bugaenko <oleksandr.buhaienko@raccoongang.com>
2019-12-23 18:05:05 +0200

    [HERA-hotfix] fix admin toolbar

--- cms/static/js/hera/components/PagesBaseComponent.jsx
+++ cms/static/js/hera/components/PagesBaseComponent.jsx
@@ -23,6 +23,17 @@ export default class PagesBaseComponent extends React.Component {
                 <div className="author-block__content">
                     <div className="author-block__image">
                         <img src={data.imgUrl} alt=""/>
+
+                        <div className="author-block__image-selector">
+                            <i className="fa fa-picture-o" aria-hidden="true"></i>
+                            <br/>
+                            <button type="button" className="author-block__image-selector__btn">
+                                + Add image
+                            </button>
+                            <button type="button" className="author-block__image-selector__btn">
+                                + Add image
+                            </button>
+                        </div>
                     </div>
                     <div className="author-block__question">
                         {data.sliderBar.map((bar, index) => {
@@ -41,14 +52,30 @@ export default class PagesBaseComponent extends React.Component {
                     </div>
                 </div>
                 <div className="author-toolbar">
-                    <input type="text" onChange={this.changeImgUrl.bind(this)} value={imgUrl}/>
-                    <button onClick={this.confirmImgUrl.bind(this)}>Confirm image url</button>
-                    <button onClick={this.cancelImgUrl.bind(this)}>Cancel</button>
+                    <div className="author-toolbar__row">
+                        <input className="author-toolbar__field" type="text" onChange={this.changeImgUrl.bind(this)} value={imgUrl} placeholder='Paste URL of the image' />
+                        <button className="author-toolbar__btn cancel" onClick={this.cancelImgUrl.bind(this)}>
+                            <i className="fa fa-trash-o" aria-hidden="true" />
+                        </button>
+                        <button className="author-toolbar__btn is-disabled" onClick={this.confirmImgUrl.bind(this)}>
+                            <i className="fa fa-check" aria-hidden="true"></i>
+                        </button>
+                        <div className="author-toolbar__add">
+                            <button className="author-toolbar__add__btn" onClick={this.confirmImgUrl.bind(this)}>
+                                + add image
+                            </button>
+                        </div>
+                    </div>
 
-                    <input type="text" onChange={this.changeIframeUrl.bind(this)} value={iframeUrl}/>
-                    <button onClick={this.confirmIframeUrl.bind(this)}>Confirm iframe url</button>
-                    <button onClick={this.cancelIframeUrl.bind(this)}>Cancel iframe url</button>
-
+                    <div className="author-toolbar__row">
+                        <input className="author-toolbar__field" type="text" onChange={this.changeIframeUrl.bind(this)} value={iframeUrl} placeholder='Paste URL of the iframe' />
+                        <button className="author-toolbar__btn cancel" onClick={this.cancelIframeUrl.bind(this)}>
+                            <i className="fa fa-trash-o" aria-hidden="true" />
+                        </button>
+                        <button className="author-toolbar__btn is-disabled" onClick={this.confirmIframeUrl.bind(this)}>
+                            <i className="fa fa-check" aria-hidden="true"></i>
+                        </button>
+                    </div>
                 </div>
                 <div className="author-block__buttons">
                     <button type="button" className="author-block__btn">
