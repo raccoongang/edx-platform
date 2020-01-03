cbebfe676de187120701b7c8f92e5afee229d52a
o.bugaenko <oleksandr.buhaienko@raccoongang.com>
2020-01-03 11:09:53 +0200

    [HERA-134] CMS markup questions and scaffolds

--- cms/static/js/hera/components/Question.jsx
+++ cms/static/js/hera/components/Question.jsx
@@ -15,6 +15,114 @@ export default class Question extends React.Component{
                     </div>
                     <div className="author-block__question">
                         {activeQuestion.content}
+                        <div className="questions__wrapper">
+                            <div className="questions__list__toolbar">
+                                <button type="button" className="questions__list__toolbar__btn">
+                                    <i className="fa fa-dot-circle-o" aria-hidden="true" />
+                                </button>
+                                <button type="button" className="questions__list__toolbar__btn">
+                                    <i className="fa fa-check-square-o" aria-hidden="true" />
+                                </button>
+                                <button type="button" className="questions__list__toolbar__btn">
+                                    <i className="fa fa-list-ol" aria-hidden="true" />
+                                </button>
+                                <button type="button" className="questions__list__toolbar__btn">
+                                    <i className="fa fa-text-width" aria-hidden="true" />
+                                </button>
+                            </div>
+                            <div className="questions__list radio">
+                                <div className="questions__list__item">
+                                    <label className="questions__list__label">
+                                        <input className="questions__list__radio" type="radio" name="radio" />
+                                        <div className="questions__list__text">Question 1</div>
+                                    </label>
+                                </div>
+
+                                <div className="questions__list__item">
+                                    <label className="questions__list__label">
+                                        <input className="questions__list__radio" type="radio" name="radio" />
+                                        <div className="questions__list__text">Question 1</div>
+                                    </label>
+                                </div>
+
+                                <div className="questions__list__item">
+                                    <label className="questions__list__label">
+                                        <input className="questions__list__radio" type="radio" name="radio" />
+                                        <div className="questions__list__text">Question 1</div>
+                                    </label>
+                                </div>
+
+                                <div className="questions__list__add-item">
+                                    <button type="button" className="questions__list__add-item__btn">
+                                        + add item
+                                    </button>
+                                </div>
+                            </div>
+
+                            <div className="questions__list checkboxes">
+                                <div className="questions__list__item">
+                                    <label className="questions__list__label">
+                                        <input className="questions__list__checkbox" type="checkbox" name="checkbox" />
+                                        <div className="questions__list__text checkbox">Question 1</div>
+                                    </label>
+                                </div>
+
+                                <div className="questions__list__item">
+                                    <label className="questions__list__label">
+                                        <input className="questions__list__checkbox" type="checkbox" name="checkbox" />
+                                        <div className="questions__list__text checkbox">Question 1</div>
+                                    </label>
+                                </div>
+
+                                <div className="questions__list__item">
+                                    <label className="questions__list__label">
+                                        <input className="questions__list__checkbox" type="checkbox" name="checkbox" />
+                                        <div className="questions__list__text checkbox">Question 1</div>
+                                    </label>
+                                </div>
+
+                                <div className="questions__list__add-item">
+                                    <button type="button" className="questions__list__add-item__btn">
+                                        + add item
+                                    </button>
+                                </div>
+                            </div>
+
+                            <div className="questions__list numerical">
+                                <div className="questions__list__item">
+                                    <input className="questions__list__numerical" type="number" />
+                                </div>
+                                <div className="questions__list__add-item">
+                                    <button type="button" className="questions__list__add-item__btn">
+                                        + add item
+                                    </button>
+                                </div>
+                            </div>
+
+                            <div className="questions__list textarea">
+                                <div className="questions__list__item">
+                                    <textarea className="questions__list__textarea" name="text"></textarea>
+                                </div>
+
+                                <div className="questions__list__add-item">
+                                    <button type="button" className="questions__list__add-item__btn">
+                                        + add item
+                                    </button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="scaffolds__wrapper">
+                            <button type="button" className="scaffolds__btn">
+                                Scaffolds 1
+                            </button>
+                            <button type="button" className="scaffolds__btn">
+                                Scaffolds 2
+                            </button>
+                            <button type="button" className="scaffolds__btn">
+                                Scaffolds 3
+                            </button>
+                        </div>
                     </div>
                 </div>
                 <div className="author-block__buttons">
