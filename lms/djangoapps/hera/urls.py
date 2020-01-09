509181b962bf79893fa6766733efaf9960c89bc4
Sagirov Eugeniy <sagirov19@gmail.com>
2020-01-09 16:31:53 +0200

    change urls for hera

--- lms/djangoapps/hera/urls.py
+++ lms/djangoapps/hera/urls.py
@@ -2,11 +2,15 @@
 URL definitions for hera app.
 """
 
+from django.conf import settings
 from django.conf.urls import url
 
 from hera import views
 
 urlpatterns = [
     url(r'^onboarding/', views.OnboardingPagesView.as_view(), name='onboarding'),
-    url(r'^selection_page/', views.SelectionPageView.as_view(), name='selection_page'),
+    url(r'^courses/{}/course/selection_page/'.format(
+            settings.COURSE_ID_PATTERN,
+        ),
+        views.SelectionPageView.as_view(), name='selection_page'),
 ]
