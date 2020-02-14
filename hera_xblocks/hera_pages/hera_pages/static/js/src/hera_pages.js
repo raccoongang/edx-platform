572b349d03b296f233760649b4a9b7506d375055
Golub-Sergey <1.golub.sergey.1@gmail.com>
2020-02-14 15:32:27 +0200

    upgrades hera_pages xblock rendering and adds table handling

--- hera_xblocks/hera_pages/hera_pages/static/js/src/hera_pages.js
+++ hera_xblocks/hera_pages/hera_pages/static/js/src/hera_pages.js
@@ -1,7 +1,21 @@
 /* Javascript for HeraPagesXBlock. */
 function HeraPagesXBlock(runtime, element) {
-
+    var submithHandlerUrl = runtime.handlerUrl(element, 'submit');
     var isFormValid;
+    var $questionForm = $(".table-form", element);
+    var $submit = $('.submit', element);
+
+    $('#slider-popup').popup();
+    $('.slidebar-wrapper, .image-wrapper').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        fade: true,
+        adaptiveHeight: true,
+        infinite: false,
+        draggable: false,
+        // asNavFor: '.slider-nav'
+      });
+
     let shouldShowMessage = true;
 
     $(".button-prev", element).click(function(e){
@@ -45,4 +59,44 @@ function HeraPagesXBlock(runtime, element) {
             // asNavFor: '.slider-nav'
         });
     });
+
+    $questionForm.on('change blur keyup', function(e) {
+        var serializedForm = $questionForm.serializeArray();
+        var emptyFormChecker = [];
+
+        $(serializedForm).each(function( index, element ) {
+            if (element.value) {
+                emptyFormChecker.push(true)
+            } else {
+                emptyFormChecker.push(false)
+            }
+        });
+
+        const every = (element) => element === true;
+
+        if (emptyFormChecker.every(every)) {
+            $submit.removeAttr("disabled");
+        } else {
+            $submit.attr("disabled", 'disabled');
+        }
+    });
+
+
+    $submit.bind('click', function (e) {
+        var serializedForm = $questionForm.serializeArray();
+        var userAnswers = [];
+
+        for (var i = 0; i < $questionForm.length; i++) {
+            userAnswers.push([]);
+        }
+        for (var i in serializedForm) {
+            userAnswers[serializedForm[i].name].push(serializedForm[i].value);
+        }
+
+        $.post(
+            submithHandlerUrl,
+            JSON.stringify({"answers": userAnswers})
+        )
+    });
+
 }
