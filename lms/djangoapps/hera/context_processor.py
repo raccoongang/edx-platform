7c0b6501fe4f97c1f5a434dcf1569260b00f73c0
Alexander Marenich <sendr84@gmail.com>
2020-02-26 17:18:09 +0200

    Models UserOnboarding and ActiveCourseSettings caching data - added caching to context_processors

--- lms/djangoapps/hera/context_processor.py
+++ lms/djangoapps/hera/context_processor.py
@@ -5,12 +5,11 @@ def user_onboarding(request):
     #TODO: need to improve this logic, ideally to get onboarding pages via ajax request 
     # in order not to request DB for onboarding data every time page loads
     if request.user.is_authenticated():
-        user_onboarding = UserOnboarding.objects.filter(user=request.user).first()
         onboarding_data = {
             'is_onboarding_passed': False,
         }
-        if user_onboarding and user_onboarding.is_passed():
+        if UserOnboarding.onboarding_is_passed(request.user.id):
             onboarding_data['is_onboarding_passed'] = True
-            onboarding_data['onboarding_pages'] = user_onboarding.get_pages()
+            onboarding_data['onboarding_pages'] = UserOnboarding.get_static_pages()
         return onboarding_data
     return {}
