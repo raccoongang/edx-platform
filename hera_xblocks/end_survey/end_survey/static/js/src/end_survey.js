7aed9f3cec96e2228d24273988db6d9c67f6fe6f
Alexander Marenich <sendr84@gmail.com>
2020-02-27 16:26:54 +0200

    HERA-501 - all done

--- hera_xblocks/end_survey/end_survey/static/js/src/end_survey.js
+++ hera_xblocks/end_survey/end_survey/static/js/src/end_survey.js
@@ -1,71 +1,76 @@
 /* Javascript for EndSurveyXBlock. */
 function EndSurveyXBlock(runtime, element, init_args) {
     var handlerUrl = runtime.handlerUrl(element, 'vote');
+    var renderHTMLHandlerUrl = runtime.handlerUrl(element, 'render_html');
+    var blockId = init_args.block_id;
+
     var $questionsForm = $(".survey-form-questions", element);
     var $confidenceForm = $(".survey-form-confidence", element);
-    var blockId = init_args.block_id;
     var sclickSliderSelector = '.slidebar-wrapper-' + blockId;
-    $(sclickSliderSelector).slick({
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        fade: true,
-        adaptiveHeight: true,
-        infinite: false
-      });
+    $.post(renderHTMLHandlerUrl, '{}').done(function(response) {
+        $('#main-content-' + blockId, element).html(response.content);
+        $(sclickSliderSelector).slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            fade: true,
+            adaptiveHeight: true,
+            infinite: false
+          });
 
-    $(".survey-button-next").click(function(e) {
-        $(sclickSliderSelector).slick('slickNext');
-    });
+        $(".survey-button-next").click(function(e) {
+            $(sclickSliderSelector).slick('slickNext');
+        });
 
-    $(".button-submit").click(function(e) {
-        $('.button-submit').prop('disabled', true);
-        const answers = $questionsForm.serializeArray();
-        const confidence = $confidenceForm.serializeArray();
-        let answersData = {};
-        for (let i in answers) {
-            let answerObj = answers[i];
-            answersData[answerObj.name] = answerObj.value;
-        };
-        let confidenceData = {};
-        confidenceData[confidence[0].name] = confidence[0].value;
-        data = {
-            answersData: answersData,
-            confidenceData: confidenceData
-        }
-        $.ajax({
-            type: "POST",
-            url: handlerUrl,
-            data: JSON.stringify(data),
-            success: function(response) {
-                $("#wrapper-page").html(response);
-                $(".button-next-" + blockId).prop('disabled', false);
+        $(".button-submit").click(function(e) {
+            $('.button-submit').prop('disabled', true);
+            const answers = $questionsForm.serializeArray();
+            const confidence = $confidenceForm.serializeArray();
+            let answersData = {};
+            for (let i in answers) {
+                let answerObj = answers[i];
+                answersData[answerObj.name] = answerObj.value;
+            };
+            let confidenceData = {};
+            confidenceData[confidence[0].name] = confidence[0].value;
+            data = {
+                answersData: answersData,
+                confidenceData: confidenceData
             }
-        })
-    });
+            $.ajax({
+                type: "POST",
+                url: handlerUrl,
+                data: JSON.stringify(data),
+                success: function(response) {
+                    $("#wrapper-page").html(response);
+                    $(".button-next-" + blockId).prop('disabled', false);
+                }
+            });
+        });
 
-    $(".possible-answers").change(function(e) {
-        if ($(".possible-answers:checked").length === $(".question-title").length) {
-            $('.survey-button-next').attr('disabled', false);
-        } else {
-            $('.survey-button-next').attr('disabled', 'disabled');
+        $(".possible-answers").change(function(e) {
+            if ($(".possible-answers:checked").length === $(".question-title").length) {
+                $('.survey-button-next').attr('disabled', false);
+            } else {
+                $('.survey-button-next').attr('disabled', 'disabled');
+            }
+        });
+        $(".confidence-answers").change(function(e) {
+            if ($(".confidence-answers:checked").length === $(".confidence-title").length) {
+                $('.button-submit').attr('disabled', false);
+            } else {
+                $('.button-submit').attr('disabled', 'disabled');
+            }
+        });
+
+        if ($('input:checked', element).length === $('.survey-holder', element).length) {
+            $('.button-next-' + blockId, element).attr('disabled', false);
         }
-    });
-    $(".confidence-answers").change(function(e) {
-        if ($(".confidence-answers:checked").length === $(".confidence-title").length) {
-            $('.button-submit').attr('disabled', false);
-        } else {
-            $('.button-submit').attr('disabled', 'disabled');
-        }
-    });
     
-    if ($('input:checked', element).length === $('.survey-holder', element).length) {
-        $('.button-next-' + blockId, element).attr('disabled', false);
-    }
-
-    $('.button-next-' + blockId, element).click(function() {
-        $('.sequence-nav-button.button-next').get(0).click();
-    });
-    $('.button-previous-' + blockId, element).click(function() {
-        $('.sequence-nav-button.button-previous').get(0).click();
+        $('.button-next-' + blockId, element).click(function() {
+            $('.sequence-nav-button.button-next').get(0).click();
+        });
+        $('.button-previous-' + blockId, element).click(function() {
+            $('.sequence-nav-button.button-previous').get(0).click();
+        });
     });
 }
