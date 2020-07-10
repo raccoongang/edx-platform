cb5043b0bfe8b2225567da936e1e52650e88a6a1
Sagirov Eugeniy <sagirov19@gmail.com>
2020-07-10 12:16:28 +0300

    code review

--- common/djangoapps/course_category/utils.py
+++ common/djangoapps/course_category/utils.py
@@ -2,7 +2,5 @@ from course_category.models import CourseCategory
 
 
 def add_categories_for_course(category_names, course_id):
-    for category_name in category_names:
-        category = CourseCategory.objects.filter(name=category_name).first()
-        if category:
-            category.courses.add(course_id)
+    for category in CourseCategory.objects.filter(name__in=category_names):
+        category.courses.add(course_id)
