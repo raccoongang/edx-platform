1bd1d896103d5bac920e6ef687c578d9f93ad324
Alexander Marenich <sendr84@gmail.com>
2020-07-17 16:36:13 +0300

    T-313 Create doc for customer (Translations) - email extended reports comes without translations.

--- common/djangoapps/tedix_ro/tasks.py
+++ common/djangoapps/tedix_ro/tasks.py
@@ -8,11 +8,12 @@ from django.conf import settings
 from django.contrib.auth.models import User
 from django.core.mail import send_mail
 from django.urls import reverse
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext as _, override as override_language
 import pytz
 
 from edxmako.shortcuts import render_to_string
 from opaque_keys.edx.keys import CourseKey
+from openedx.core.djangoapps.lang_pref import LANGUAGE_KEY
 from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
 from xmodule.modulestore.django import modulestore
 
@@ -48,29 +49,31 @@ def send_teacher_extended_reports():
                         reverse('extended_report', kwargs={'course_key': course.id})
                     )
                 })
-        if lesson_reports:
-            subject = _(u'{platform_name}: Report for {username}').format(
-                platform_name=settings.PLATFORM_NAME,
-                username=instructor.user.profile.name or instructor.user.username
-            )
-            context = {
-                'lms_url': configuration_helpers.get_value('LMS_ROOT_URL', settings.LMS_ROOT_URL),
-                'user': instructor.user,
-                'lesson_reports': lesson_reports
-            }
-            html_message = render_to_string(
-                'emails/extended_report_mail.html',
-                context
-            )
-            txt_message = render_to_string(
-                'emails/extended_report_mail.txt',
-                context
-            )
-            from_address = configuration_helpers.get_value(
-                'email_from_address',
-                settings.DEFAULT_FROM_EMAIL
-            )
-            send_mail(subject, txt_message, from_address, [instructor.user.email], html_message=html_message)
+        user_language = instructor.user.preferences.model.get_value(user, LANGUAGE_KEY, default=settings.LANGUAGE_CODE)
+        with override_language(user_language):
+            if lesson_reports:
+                subject = _(u'{platform_name}: Report for {username}').format(
+                    platform_name=settings.PLATFORM_NAME,
+                    username=instructor.user.profile.name or instructor.user.username
+                )
+                context = {
+                    'lms_url': configuration_helpers.get_value('LMS_ROOT_URL', settings.LMS_ROOT_URL),
+                    'user': instructor.user,
+                    'lesson_reports': lesson_reports
+                }
+                html_message = render_to_string(
+                    'emails/extended_report_mail.html',
+                    context
+                )
+                txt_message = render_to_string(
+                    'emails/extended_report_mail.txt',
+                    context
+                )
+                from_address = configuration_helpers.get_value(
+                    'email_from_address',
+                    settings.DEFAULT_FROM_EMAIL
+                )
+                send_mail(subject, txt_message, from_address, [instructor.user.email], html_message=html_message)
 
 
 @periodic_task(run_every=crontab(hour='15', minute='30'))
@@ -122,11 +125,13 @@ def send_student_extended_reports(user_id, course_id):
     Sends email and SMS with an extended report for student and his parent
     """
     user = User.objects.filter(id=user_id).first()
-    if user and hasattr(user, 'studentprofile'):
-        course_key = CourseKey.from_string(course_id)
-        course = modulestore().get_course(course_key)
-        header, user_data = report_data_preparation(user, course)
-        lesson_reports = [{
+    user_language = user.preferences.model.get_value(user, LANGUAGE_KEY, default=settings.LANGUAGE_CODE)
+    with override_language(user_language):
+        if user and hasattr(user, 'studentprofile'):
+            course_key = CourseKey.from_string(course_id)
+            course = modulestore().get_course(course_key)
+            header, user_data = report_data_preparation(user, course)
+            lesson_reports = [{
                 'course_name': course.display_name,
                 'report_data': [user_data,],
                 'report_url': urljoin(
@@ -134,46 +139,46 @@ def send_student_extended_reports(user_id, course_id):
                     reverse('extended_report', kwargs={'course_key': course.id})
                 )
             }]
-        subject = _(u'{platform_name}: Report for {username} on "{course_name}"').format(
-            platform_name=settings.PLATFORM_NAME,
-            username=user.profile.name or user.username,
-            course_name=course.display_name
-        )
-        context = {
-            'lms_url': configuration_helpers.get_value('LMS_ROOT_URL', settings.LMS_ROOT_URL),
-            'user': user,
-            'lesson_reports': lesson_reports
-        }
-        html_message = render_to_string(
-            'emails/extended_report_mail.html',
-            context
-        )
-        txt_message = render_to_string(
-            'emails/extended_report_mail.txt',
-            context
-        )
-        from_address = configuration_helpers.get_value(
-            'email_from_address',
-            settings.DEFAULT_FROM_EMAIL
-        )
-        parent = user.studentprofile.parents.first()
-        send_mail(subject, txt_message, from_address, [user.email, parent.user.email], html_message=html_message)
+            subject = _(u'{platform_name}: Report for {username} on "{course_name}"').format(
+                platform_name=settings.PLATFORM_NAME,
+                username=user.profile.name or user.username,
+                course_name=course.display_name
+            )
+            context = {
+                'lms_url': configuration_helpers.get_value('LMS_ROOT_URL', settings.LMS_ROOT_URL),
+                'user': user,
+                'lesson_reports': lesson_reports
+            }
+            html_message = render_to_string(
+                'emails/extended_report_mail.html',
+                context
+            )
+            txt_message = render_to_string(
+                'emails/extended_report_mail.txt',
+                context
+            )
+            from_address = configuration_helpers.get_value(
+                'email_from_address',
+                settings.DEFAULT_FROM_EMAIL
+            )
+            parent = user.studentprofile.parents.first()
+            send_mail(subject, txt_message, from_address, [user.email, parent.user.email], html_message=html_message)
 
-        student_course_due_date = StudentCourseDueDate.objects.filter(
-            student=user.studentprofile,
-            course_id=course_key
-        ).first()
-        if student_course_due_date:
-            due_date = format_datetime(student_course_due_date.due_date, "yy.MM.dd hh:mm a", locale='en')
-        else:
-            due_date = 'N/A'
-        context.update({
-            'due_date': due_date,
-        })
-        sms_message = render_to_string(
-            'sms/light_report.txt',
-            context
-        )
-        sms_client = SMSClient()
-        sms_client.send_message(user.studentprofile.phone, sms_message)
-        sms_client.send_message(parent.phone, sms_message)
+            student_course_due_date = StudentCourseDueDate.objects.filter(
+                student=user.studentprofile,
+                course_id=course_key
+            ).first()
+            if student_course_due_date:
+                due_date = format_datetime(student_course_due_date.due_date, "yy.MM.dd hh:mm a", locale='en')
+            else:
+                due_date = 'N/A'
+            context.update({
+                'due_date': due_date,
+            })
+            sms_message = render_to_string(
+                'sms/light_report.txt',
+                context
+            )
+            sms_client = SMSClient()
+            sms_client.send_message(user.studentprofile.phone, sms_message)
+            sms_client.send_message(parent.phone, sms_message)
