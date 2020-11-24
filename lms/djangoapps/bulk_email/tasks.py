6b697b69691005ef7565a73f700461efdf88400f
mykola-s <67786562+mykola-s@users.noreply.github.com>
2020-11-24 20:17:29 +0200

    [OP-248] Bug. Change URL for link (#2257)
    * added LMS_ROOT_URL from site configuration
    * revert 'account_settings_url'

--- lms/djangoapps/bulk_email/tasks.py
+++ lms/djangoapps/bulk_email/tasks.py
@@ -102,11 +102,9 @@ def _get_course_email_context(course):
     course_title = course.display_name
     course_end_date = get_default_time_display(course.end)
     course_root = reverse('course_root', kwargs={'course_id': course_id})
-    course_url = '{}{}'.format(
-        configuration_helpers.get_value('LMS_ROOT_URL', settings.LMS_ROOT_URL),
-        course_root
-    )
-    image_url = u'{}{}'.format(settings.LMS_ROOT_URL, course_image_url(course))
+    lms_root_url = configuration_helpers.get_value('LMS_ROOT_URL', settings.LMS_ROOT_URL)
+    course_url = '{}{}'.format(lms_root_url, course_root)
+    image_url = u'{}{}'.format(lms_root_url, course_image_url(course))
     email_context = {
         'course_title': course_title,
         'course_root': course_root,
@@ -114,8 +112,8 @@ def _get_course_email_context(course):
         'course_url': course_url,
         'course_image_url': image_url,
         'course_end_date': course_end_date,
-        'account_settings_url': '{}{}'.format(settings.LMS_ROOT_URL, reverse('account_settings')),
-        'email_settings_url': '{}{}'.format(settings.LMS_ROOT_URL, reverse('dashboard')),
+        'account_settings_url': '{}{}'.format(lms_root_url, reverse('account_settings')),
+        'email_settings_url': '{}{}'.format(lms_root_url, reverse('dashboard')),
         'platform_name': configuration_helpers.get_value('PLATFORM_NAME', settings.PLATFORM_NAME),
     }
     return email_context
