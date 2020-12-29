9794e887fbd9f8275d22124faa5c4d1de856001c
opersian_rg <olena.persianova@raccoongang.com>
2020-12-29 13:34:06 +0200

    FLS-499: v2

--- lms/static/js/index_discovery/index_discovery_factory.js
+++ lms/static/js/index_discovery/index_discovery_factory.js
@@ -2,8 +2,7 @@
     'use strict';
 
     define(
-        ['jquery',
-        'backbone',
+        ['backbone',
         'js/index_discovery/models/index_search_state',
         'js/index_discovery/collections/index_filters',
         'js/index_discovery/views/index_search_form',
@@ -14,7 +13,6 @@
         'js/index_discovery/views/index_filter_bar',
         'js/index_discovery/views/index_refine_sidebar'],
         function(
-            $,
             Backbone,
             IndexSearchState,
             IndexFilters,
@@ -26,81 +24,6 @@
             IndexFilterBar,
             IndexRefineSidebar
         ) {
-            /**
-            * Sort a group of courses as per custom rules.
-            *
-            * Workaround for FLS-499.
-            */
-            const reorderCourses = function() {
-                const COURSES_ORDER = [
-                 "course-v1:Lakeside+TE101+2019",  // The Essentials of Trauma - The Opioid Crisis
-                 "course-v1:NeuroLogic+NL101+2020Q2",  // The Trauma-Informed Classroom
-                 "course-v1:Neurologic+NL105+2021_T1", // Resilience: Increasing Students' Tolerance for Stress
-                 "course-v1:LakesideGlobalInstitute+2000+2020_T1",  // Enhancing Trauma Awareness
-                 "course-v1:Lakeside+PRE1101+2020",  // PRESENCE - Introductory Track
-                 "course-v1:Lakeside+PRE1101+2020_T2",  // PRESENCE-Leadership Track
-                 "course-v1:Lakeside+PRE1103+2029_T2",  // PRESENCE - Clinical Track
-                ]
-
-                for (let j = 0; j < $(".courses-listing").children.length; j++) {
-
-                 const el = $(".courses-listing")[j];
-                 const firstCoursesHtml = [];
-                 const remainingCoursesHtml = [];
-                 let allCourses = [];
-
-                 const getCourseKey = function (element) {
-                   // e.g. "/courses/course-v1:Lakeside+PRE1103+2029_T2/about"
-                   let href = element.childNodes[1].childNodes[1].attributes["href"];
-                   if (typeof (href) === "object") {
-                     // Not sure why object in some cases
-                     href = String(href.value);
-                   }
-                   let courseKey = null;
-                   const courseKeyMatch = href.match(/(?<=courses\/)(.*?)(?=\/)/);
-                   if (courseKeyMatch === null) {
-                     // Might not be a course (a product, probably).
-                     // Shouldn't run into this in the courses-listing section of interest.
-                     return;
-                   } else {
-                     courseKey = courseKeyMatch[0];
-                   }
-                   // e.g. "course-v1:Lakeside+PRE1103+2029_T2"
-                   return courseKey;
-                 }
-
-                 // Add all the courses from the current courses-listing, excluding the ones configured in COURSES_ORDER
-                 for (let i = 0; i < el.children.length; i++) {
-                   const li = el.children[i];
-                   const courseKey = getCourseKey(li);
-                   if (COURSES_ORDER.indexOf(courseKey) === -1) {
-                     remainingCoursesHtml.push("<li class='courses-listing-item' test-attr='attr'>" + li.innerHTML + "</li>");
-                   }
-                 }
-
-                 // Populate the first batch of courses (the one we want sorted as per COURSES_ORDER)
-                 for (let k = 0; k < COURSES_ORDER.length; k++) {
-                   for (let i = 0; i < el.children.length; i++) {
-                     const li = el.children[i];
-                     const courseKey = getCourseKey(li);
-                     if (courseKey === COURSES_ORDER[k]) {
-                       firstCoursesHtml.push("<li class='courses-listing-item' test-attr='attr'>" + li.innerHTML + "</li>");
-                     }
-                   }
-
-                 }
-
-                 // Replace the courses-listing section's content with a new one
-                 if (firstCoursesHtml.length > 0) {
-                   allCourses = firstCoursesHtml.concat(remainingCoursesHtml);
-                   el.innerHTML = allCourses.join("");
-                 } else {
-                   console.info("Didn't sort a certain group of courses, and it's fine.")
-                 }
-
-                }
-            }
-
             return function(meanings, searchQuery, userLanguage, userTimezone) {
                 var dispatcher = _.extend({}, Backbone.Events);
                 var searchState = new IndexSearchState();
@@ -130,7 +53,6 @@
                     filters.reset();
                     searchForm.showLoadingIndicator();
                     searchState.performSearch(query, filters.getTerms());
-                    //reorderCourses();
                 });
 
                 dispatcher.listenTo(refineSidebar, 'selectOption', function(type, query, name) {
@@ -141,7 +63,6 @@
                         filters.add({type: type, query: query, name: name});
                         searchState.refineSearch(filters.getTerms());
                     }
-                    //reorderCourses();
                 });
 
                 dispatcher.listenTo(filterBar, 'clearFilter', removeFilter);
@@ -177,7 +98,7 @@
                     searchForm.hideLoadingIndicator();
                     listing.render();
                     refineSidebar.render();
-                    reorderCourses();
+                    //reorderCourses();
                 });
 
                 dispatcher.listenTo(searchState, 'error', function() {
@@ -188,13 +109,13 @@
                 dispatcher.listenTo(facetFilterForm.facetFilterInput, 'filter', function(filterWord) {
                     searchState.filterFacets(filterWord);
                     refineSidebar.render();
-                    reorderCourses();
+                    //reorderCourses();
                 });
 
                 dispatcher.listenTo(facetFilterForm.facetFilterInput, 'resetFilter', function() {
                     searchState.resetFacets();
                     refineSidebar.render();
-                    reorderCourses();
+                    //reorderCourses();
                 });
 
                 // kick off search on page refresh
