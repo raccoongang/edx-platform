7c5b77d5d9b40640d2e3d8ceaa3c78ee292d3e4b
Andrey Kryachko <andrey.kryachko@raccoongang.com>
2020-12-31 10:29:01 +0200

    [SKILLONOMY-339] added GOOGLE_TAG_MANAGER_ID to devstack.py

--- lms/envs/devstack.py
+++ lms/envs/devstack.py
@@ -277,3 +277,4 @@ if os.path.isfile(join(dirname(abspath(__file__)), 'private.py')):
 MODULESTORE = convert_module_store_setting_if_needed(MODULESTORE)
 
 SECRET_KEY = '85920908f28904ed733fe576320db18cabd7b6cd'
+GOOGLE_TAG_MANAGER_ID = None
