272380353e92f2f512839a46500d505ca9ddb609
Max Sokolski <cmltawt0@gmail.com>
2021-02-23 22:03:56 +0200

    Enable comprehensive theming by default for devstack

--- cms/envs/devstack_docker.py
+++ cms/envs/devstack_docker.py
@@ -1,3 +1,13 @@
 """ Overrides for Docker-based devstack. """
 
 from .devstack import *  # pylint: disable=wildcard-import, unused-wildcard-import
+from .common import _make_mako_template_dirs
+
+
+ENABLE_COMPREHENSIVE_THEMING = True
+COMPREHENSIVE_THEME_DIRS = [
+    "/edx/app/edx-themes/"
+]
+DEFAULT_SITE_THEME = 'edx-theme'
+TEMPLATES[1]["DIRS"] = _make_mako_template_dirs
+derive_settings(__name__)
--- lms/envs/devstack_docker.py
+++ lms/envs/devstack_docker.py
@@ -7,3 +7,13 @@ still refers to it.
 """
 
 from .devstack import *  # pylint: disable=wildcard-import, unused-wildcard-import
+from .common import _make_mako_template_dirs
+
+
+ENABLE_COMPREHENSIVE_THEMING = True
+COMPREHENSIVE_THEME_DIRS = [
+    "/edx/app/edx-themes/"
+]
+DEFAULT_SITE_THEME = 'edx-theme'
+TEMPLATES[1]["DIRS"] = _make_mako_template_dirs
+derive_settings(__name__)
