9f7ee0f95e392aeae699ac68842656330ed30c01
ViktorRusakov <viktor.rusakov@raccoongang.com>
2021-08-02 13:53:18 +0300

    fix: handle race condition for milestones

--- common/djangoapps/util/milestones_helpers.py
+++ common/djangoapps/util/milestones_helpers.py
@@ -3,6 +3,7 @@
 Utility library for working with the edx-milestones app
 """
 from django.conf import settings
+from django.db import IntegrityError
 from django.utils.translation import ugettext as _
 from milestones import api as milestones_api
 from milestones.exceptions import InvalidMilestoneRelationshipTypeException, InvalidUserException
@@ -427,7 +428,11 @@ def add_user_milestone(user, milestone):
     """
     if not settings.FEATURES.get('MILESTONES_APP'):
         return None
-    return milestones_api.add_user_milestone(user, milestone)
+    try:
+        return milestones_api.add_user_milestone(user, milestone)
+    # to handle rare cases when milestone is already created
+    except IntegrityError:
+        pass
 
 
 def remove_user_milestone(user, milestone):
