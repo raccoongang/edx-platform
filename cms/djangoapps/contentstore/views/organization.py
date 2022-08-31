3bea49ece42d5b793dc14dde049ca3080ca10987
ruzniaievdm <ruzniaievdm@gmail.com>
2022-08-31 19:25:30 +0300

    fix: display available libraries for user

--- cms/djangoapps/contentstore/views/organization.py
+++ cms/djangoapps/contentstore/views/organization.py
@@ -7,6 +7,7 @@ from django.utils.decorators import method_decorator
 from django.views.generic import View
 from organizations.api import get_organizations
 
+from cms.djangoapps.course_creators.models import CourseCreator
 from openedx.core.djangolib.js_utils import dump_js_escaped_json
 
 
@@ -20,6 +21,15 @@ class OrganizationListView(View):
     @method_decorator(login_required)
     def get(self, request, *args, **kwargs):  # lint-amnesty, pylint: disable=unused-argument
         """Returns organization list as json."""
+        org_names_list = []
+
+        course_creator = CourseCreator.objects.filter(user=request.user).last()
+        is_state_granted = course_creator and course_creator.state == CourseCreator.GRANTED
         organizations = get_organizations()
-        org_names_list = [(org["short_name"]) for org in organizations]
+
+        if request.user.is_staff or (is_state_granted and course_creator.all_organizations):
+            org_names_list = [(org["short_name"]) for org in organizations]
+        elif is_state_granted:
+            org_names_list = list(course_creator.member_of_organizations_name())
+
         return HttpResponse(dump_js_escaped_json(org_names_list), content_type='application/json; charset=utf-8')  # lint-amnesty, pylint: disable=http-response-with-content-type-json
