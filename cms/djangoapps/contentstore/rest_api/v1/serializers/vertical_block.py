96ee6d5899166ddde544582ce464bbcb3f60b2e8
ruzniaievdm <ruzniaievdm@gmail.com>
2024-02-06 19:32:43 +0200

    feat: [AXIMST-467] Manage Tags on block level

--- cms/djangoapps/contentstore/rest_api/v1/serializers/vertical_block.py
+++ cms/djangoapps/contentstore/rest_api/v1/serializers/vertical_block.py
@@ -123,6 +123,8 @@ class ChildVerticalContainerSerializer(serializers.Serializer):
         """
 
         can_manage_tags = use_tagging_taxonomy_list_page()
+
+        # temporary decision defining the default value 'True' for each xblock.
         actions = {
             "can_manage_tags": can_manage_tags,
         }
