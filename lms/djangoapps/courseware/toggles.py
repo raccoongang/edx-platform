747c4d1239dfa102021f547ea246bbe46a04ac41
NiedielnitsevIvan <81557788+NiedielnitsevIvan@users.noreply.github.com>
2024-03-14 16:37:06 +0200

    feat: [AXIMST-629] waffle flag for displaying discussions tab in … (#2514)
    * feat: [AXIMST-629] new waffle flag for displaying discussions tab in learning MFE by default
    
    * test: [AXIMST-629] add tests for new waffle flag view
    
    * style: [AXIMST-629] improve flag naming

--- lms/djangoapps/courseware/toggles.py
+++ lms/djangoapps/courseware/toggles.py
@@ -81,6 +81,19 @@ COURSEWARE_MICROFRONTEND_SIDEBAR_DISABLED = CourseWaffleFlag(
     f'{WAFFLE_FLAG_NAMESPACE}.disable_navigation_sidebar', __name__
 )
 
+# .. toggle_name: courseware.disable_default_opening_discussion_sidebar
+# .. toggle_implementation: WaffleFlag
+# .. toggle_default: False
+# .. toggle_description: Disable opening the discussion sidebar by default on Learning MFE.
+# .. toggle_use_cases: temporary
+# .. toggle_creation_date: 2024-03-13
+# .. toggle_target_removal_date: None
+# .. toggle_tickets: AXIMST-629
+# .. toggle_warning: None.
+COURSEWARE_MICROFRONTEND_DISCUSSION_SIDEBAR_OPEN_DISABLED = CourseWaffleFlag(
+    f'{WAFFLE_FLAG_NAMESPACE}.disable_default_opening_discussion_sidebar', __name__
+)
+
 # .. toggle_name: courseware.mfe_progress_milestones_streak_discount_enabled
 # .. toggle_implementation: CourseWaffleFlag
 # .. toggle_default: False
@@ -206,3 +219,10 @@ def courseware_mfe_sidebar_is_disabled(course_key=None):
     Return whether the courseware.disable_navigation_sidebar flag is on.
     """
     return COURSEWARE_MICROFRONTEND_SIDEBAR_DISABLED.is_enabled(course_key)
+
+
+def courseware_mfe_discussion_sidebar_opening_is_disabled(course_key=None):
+    """
+    Return whether the courseware.disable_default_opening_discussion_sidebar flag is on.
+    """
+    return COURSEWARE_MICROFRONTEND_DISCUSSION_SIDEBAR_OPEN_DISABLED.is_enabled(course_key)
