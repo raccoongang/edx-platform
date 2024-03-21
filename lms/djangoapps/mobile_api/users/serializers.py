d7652e9612bf1f2f92d8792fc909e41996e1f596
Іван Нєдєльніцев <ivan.niedielnitsev@raccoongang.com>
2024-03-21 15:24:56 +0200

    feat: [AXM-40] add courses progress to enrollment endpoint

--- lms/djangoapps/mobile_api/users/serializers.py
+++ lms/djangoapps/mobile_api/users/serializers.py
@@ -4,6 +4,7 @@ Serializer for user API
 
 from typing import Dict, List, Optional, Tuple
 
+from django.core.cache import cache
 from completion.exceptions import UnavailableCompletionData
 from completion.utilities import get_key_to_last_completed_block
 from rest_framework import serializers
@@ -17,6 +18,8 @@ from lms.djangoapps.courseware.access import has_access
 from lms.djangoapps.courseware.block_render import get_block_for_descriptor
 from lms.djangoapps.courseware.courses import get_current_child
 from lms.djangoapps.courseware.model_data import FieldDataCache
+from lms.djangoapps.grades.api import CourseGradeFactory
+from openedx.core.djangoapps.content.block_structure.api import get_block_structure_manager
 from openedx.features.course_duration_limits.access import get_user_course_expiration_date
 from xmodule.modulestore.django import modulestore
 
@@ -91,7 +94,47 @@ class CourseOverviewField(serializers.RelatedField):  # lint-amnesty, pylint: di
         }
 
 
-class CourseEnrollmentSerializer(serializers.ModelSerializer):
+class CourseEnrollmentProgressMixin(serializers.Serializer):
+    """
+    Mixin for CourseEnrollment serializers to add progress information.
+    """
+
+    progress = serializers.SerializerMethodField()
+
+    BLOCK_STRUCTURE_CACHE_TIMEOUT = 60 * 60  # 1 hour
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.context.get('api_version') == 'v4':
+            self.progress = serializers.SerializerMethodField()
+            self.Meta.fields = list(self.Meta.fields)
+            self.Meta.fields.append('progress')
+
+    def get_progress(self, model: CourseEnrollment) -> Dict[str, int]:
+        """
+        Returns the progress of the user in the course.
+        """
+        assert isinstance(model, CourseEnrollment), f'Expected CourseEnrollment, got {type(model)}'
+        is_staff = bool(has_access(model.user, 'staff', model.course.id))
+
+        cache_key = f'course_block_structure_{str(model.course.id)}_{model.user.id}'
+        collected_block_structure = cache.get(cache_key)
+        if not collected_block_structure:
+            collected_block_structure = get_block_structure_manager(model.course.id).get_collected()
+            cache.set(cache_key, collected_block_structure, self.BLOCK_STRUCTURE_CACHE_TIMEOUT)
+
+        course_grade = CourseGradeFactory().read(model.user, collected_block_structure=collected_block_structure)
+
+        # recalculate course grade from visible grades (stored grade was calculated over all grades, visible or not)
+        course_grade.update(visible_grades_only=True, has_staff_access=is_staff)
+        subsection_grades = list(course_grade.subsection_grades.values())
+        return {
+            'num_points_earned': sum(map(lambda x: x.graded_total.earned if x.graded else 0, subsection_grades)),
+            'num_points_possible': sum(map(lambda x: x.graded_total.possible if x.graded else 0, subsection_grades)),
+        }
+
+
+class CourseEnrollmentSerializer(serializers.ModelSerializer, CourseEnrollmentProgressMixin):
     """
     Serializes CourseEnrollment models
     """
