91ed1d8c84a342b670832803de29c965d983d970
NiedielnitsevIvan <81557788+NiedielnitsevIvan@users.noreply.github.com>
2024-04-02 12:15:46 +0300

    feat: [AXIMST-696] Add calculation for percentage of the completions (#2523)
    * feat: [AXIMST-696] Add calculation for percentage of the completions
    
    * test: [AXIMST-696] add test for completion_stat functionality

--- lms/djangoapps/course_home_api/outline/views.py
+++ lms/djangoapps/course_home_api/outline/views.py
@@ -459,9 +459,7 @@ class CourseSidebarBlocksView(RetrieveAPIView):
         )
         if courseware_mfe_navigation_sidebar_blocks_caching_is_enabled():
             course_blocks = cache.get(cache_key)
-            cached = course_blocks is not None
         else:
-            cached = False
             course_blocks = None
 
         if not course_blocks:
@@ -474,15 +472,13 @@ class CourseSidebarBlocksView(RetrieveAPIView):
                 cache.set(cache_key, course_blocks, self.COURSE_BLOCKS_CACHE_TIMEOUT)
 
         course_blocks = self.filter_unavailable_blocks(course_blocks, course_key)
-
-        if cached:
-            # If the data was cached, we need to mark the blocks as complete or not complete.
+        if course_blocks:
             course_blocks = self.mark_complete_recursive(course_blocks)
 
         context = self.get_serializer_context()
         context.update({
             'include_vertical': True,
-            'extra_fields': ['special_exam_info'],
+            'extra_fields': ['special_exam_info', 'completion_stat'],
             'enable_prerequisite_block_type': True,
         })
 
@@ -502,7 +498,7 @@ class CourseSidebarBlocksView(RetrieveAPIView):
             for section_data in course_sections:
                 section_data['children'] = self.get_available_sequences(
                     user_course_outline,
-                    section_data.get('children', [])
+                    section_data.get('children', ['completion'])
                 )
                 accessible_sequence_ids = {str(usage_key) for usage_key in user_course_outline.accessible_sequences}
                 for sequence_data in section_data['children']:
@@ -516,10 +512,42 @@ class CourseSidebarBlocksView(RetrieveAPIView):
         """
         if 'children' in block:
             block['children'] = [self.mark_complete_recursive(child) for child in block['children']]
-            block['complete'] = all(child['complete'] for child in block['children'] if child['type'] != 'discussion')
+            completable_children = self.get_completable_children(block)
+            block['complete'] = completable_children == [] or all(child['complete'] for child in completable_children)
+            block['completion_stat'] = self.get_block_completion_stat(block, completable_children)
         else:
-            block['complete'] = self.completions_dict.get(block['id'], False)
+            # If the block is a course, chapter, sequential, or vertical, without children,
+            # it should be completed by default.
+            completion = self.completions_dict.get(block['id'], 0)
+            block['complete'] = bool(completion) or block['type'] in ['course', 'chapter', 'sequential', 'vertical']
+            block['completion_stat'] = self.get_block_completion_stat(block, completable_children=[])
+
         return block
+
+    def get_block_completion_stat(self, block, completable_children):
+        """
+        Get the completion status of a block.
+        """
+        block_type = block['type']
+
+        if block_type in ['course', 'chapter', 'sequential']:
+            completion = sum(child['complete'] for child in completable_children)
+        elif block_type == 'vertical':
+            completion = sum(child['completion_stat']['completion'] for child in completable_children)
+        else:
+            completion = self.completions_dict.get(block['id'], 0)
+
+        return {
+            'completion': completion,
+            'completable_children': len(completable_children),
+        }
+
+    @staticmethod
+    def get_completable_children(block):
+        """
+        Get the completable children of a block.
+        """
+        return [child for child in block.get('children', []) if child['type'] != 'discussion']
 
     @staticmethod
     def get_available_sections(user_course_outline, course_sections):
