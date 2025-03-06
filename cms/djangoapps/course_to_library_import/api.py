1c8825bab1bfd1c52d0c6d4c799f0aeaa5ce52d4
Іван Нєдєльніцев <ivan.niedielnitsev@raccoongang.com>
2025-03-06 17:24:59 +0200

    feat: create MVP of course to library import

--- cms/djangoapps/course_to_library_import/api.py
+++ cms/djangoapps/course_to_library_import/api.py
@@ -2,21 +2,209 @@
 Python API for course to library import
 =======================================
 """
+import logging
+import mimetypes
+from collections import ChainMap
+from datetime import datetime, timezone
+
+from django.db import transaction
+from django.shortcuts import get_object_or_404
+from django.contrib.auth import get_user_model
+from lxml import etree
+
+from opaque_keys.edx.keys import CourseKey, UsageKey
+from opaque_keys.edx.locator import LibraryLocatorV2
+from openedx_events.content_authoring.signals import LIBRARY_BLOCK_CREATED
+from openedx_events.content_authoring.data import LibraryBlockData
+from openedx_learning.api import authoring as authoring_api
+
+from openedx.core.djangoapps.content_libraries import api, permissions
+from openedx.core.djangoapps.content_staging import api as content_staging_api
+from openedx.core.djangoapps.content_staging.models import StagedContent
+from xmodule.modulestore.django import modulestore
 
 from .models import CourseToLibraryImport
 
+log = logging.getLogger(__name__)
+COURSE_TO_LIBRARY_IMPORT_PURPOSE = 'course_to_library_import'
+User = get_user_model()
 
-def create_import(course_key, library_key, source_type):
+def save_course_to_staged_content(course_id, user_id, purpose=None, version_num=None):
+    """
+
+    :param user_id:
+    :param course_id:
+    :param purpose:
+    :param version_num:
+    :return:
+    """
+    course_key = CourseKey.from_string(course_id)
+    sections = modulestore().get_items(course_key, qualifiers={'category': 'chapter'})
+    for section in sections:
+        content_staging_api.stage_xblock_temporarily(
+            section,
+            user_id,
+            purpose=purpose or COURSE_TO_LIBRARY_IMPORT_PURPOSE,
+            version_num=version_num
+        )
+
+
+def import_library_from_staged_content(library_key, user_id, usage_ids):
+    """
+
+    :param library_key:
+    :param user_id:
+    :param usage_ids:
+    :return:\
+    """
+    # Get staged content with user id status READY and purpose COURSE_TO_LIBRARY_IMPORT_PURPOSE
+    # validate usage_ids in staged content tags
+    # parse staged content OLX
+    # iter for child of section, validate OLX for unit and xblock level
+    # import OLX to library q
+    # ref import_staged_content_from_user_clipboard,
+    # + create ComponentVersionImport objects
+    # + update CourseToLibraryImport status to IMPORTED
+    # USE TRANSACTION!
+    staged_content = StagedContent.objects.filter(
+        user_id=user_id,
+        status='Ready',
+        purpose=COURSE_TO_LIBRARY_IMPORT_PURPOSE,
+    )
+    validate_usage_ids(usage_ids, staged_content)
+    parser = etree.XMLParser(strip_cdata=False)
+    library_key = LibraryLocatorV2.from_string(library_key)
+    imported = False
+    for usage_key in usage_ids:
+        if staged_content_item := staged_content.filter(tags__icontains=usage_key).first():
+            node = etree.fromstring(staged_content_item.olx, parser=parser)
+            usage_key = UsageKey.from_string(usage_key)
+            block_to_import = get_block_to_import(node, usage_key)
+            if not block_to_import:
+                continue
+            if block_to_import.tag in ('chapter', 'sequential', 'vertical'):
+                # TODO: add support different composition
+                flat_import_children(block_to_import, library_key, user_id, staged_content_item.id)
+            else:
+                # validate_can_add_block_to_library(library_key, block_to_import.tag, block_to_import.get('url_name'))
+                create_block_in_library(block_to_import, usage_key, library_key, user_id, staged_content_item.id)
+            imported = True
+
+    if imported:
+        CourseToLibraryImport.objects.filter(
+            user_id=user_id,
+            library_key=library_key
+        ).update(status=CourseToLibraryImport.IMPORTED)
+
+
+def flat_import_children(block_to_import, content_library, user_id, staged_content):
+    for child in block_to_import.getchildren():
+        if child.tag in ('chapter', 'sequential', 'vertical'):
+            flat_import_children(child, content_library, user_id, staged_content.id)
+        else:
+            usage_key_fragment = f"type@{child.tag}+block@{child.get('url_name')}"
+            usage_key = None
+            for tag_key in staged_content.tags.keys():
+                if usage_key_fragment in tag_key:
+                    usage_key = UsageKey.from_string(child.get('url_name'))  # FIXME find better way to get usage_key
+                    break
+            if usage_key:
+                create_block_in_library(child, usage_key, content_library, user_id, staged_content.id)
+
+
+def create_block_in_library(block_to_import, usage_key, library_key, user_id, staged_content_id):
+
+    now = datetime.now(tz=timezone.utc)
+    staged_content_files = content_staging_api.get_staged_content_static_files(staged_content_id)
+    content_library, library_usage_key = api.validate_can_add_block_to_library(
+        library_key,
+        block_to_import.tag,
+        usage_key.block_id,
+    )
+
+    with transaction.atomic():
+        component_type = authoring_api.get_or_create_component_type("xblock.v1", usage_key.block_type)
+        authoring_api.create_component(
+            content_library.learning_package.id,
+            component_type=component_type,
+            local_key=usage_key.block_id,
+            created=now,
+            created_by=user_id,
+        )
+        component_version = api.set_library_block_olx(library_usage_key, etree.tostring(block_to_import))
+
+        for staged_content_file_data in staged_content_files:
+            filename = staged_content_file_data.filename
+            file_data = content_staging_api.get_staged_content_static_file_data(
+                staged_content_id,
+                filename,
+            )
+            if not file_data:
+                log.error(
+                    f"Staged content {staged_content_id} included referenced "
+                    f"file {filename}, but no file data was found."
+                )
+                continue
+
+            filename = f"static/{str(usage_key)}"
+
+            media_type_str, _encoding = mimetypes.guess_type(filename)
+            if not media_type_str:
+                media_type_str = "application/octet-stream"
+
+            media_type = authoring_api.get_or_create_media_type(media_type_str)
+            content = authoring_api.get_or_create_file_content(
+                content_library.learning_package.id,
+                media_type.id,
+                data=file_data,
+                created=now,
+            )
+
+            # authoring_api.create_component_version_content(
+            #     component_version.pk,
+            #     content.id,
+            #     key=filename,
+            # )
+
+    LIBRARY_BLOCK_CREATED.send_event(
+        library_block=LibraryBlockData(
+            library_key=content_library.library_key,
+            usage_key=usage_key
+        )
+    )
+
+
+def get_block_to_import(node, usage_key):
+    if node.get('url_name') == usage_key.block_id:
+        return node
+
+    for child in node.getchildren():
+        if found := get_block_to_import(child, usage_key):
+            return found
+
+
+def validate_usage_ids(usage_ids, staged_content):
+    available_block_keys = ChainMap(*staged_content.values_list('tags', flat=True))
+    for usage_key in usage_ids:
+        if usage_key not in available_block_keys:
+            raise ValueError(f'Block {usage_key} is not available for import')
+
+
+def create_import(course_ids, user_id, library_key, source_type):
     """
     Create a new import task to import a course to a library.
 
     Args:
-        course_key (str): The course key of the course to import.
+        course_ids (str): The course key of the course to import.
+        user_id (int): The ID of the user who initiated the import.
         library_key (str): The key of the library to import the course to.
         source_type (str): The type of the source of the import.
     """
-    import_task = CourseToLibraryImport(
-        course_id=course_key,
+    for course_id in course_ids:
+        save_course_to_staged_content(course_id, user_id, COURSE_TO_LIBRARY_IMPORT_PURPOSE)
+
+    import_task = CourseToLibraryImport(  # FIXME: args
+        course_id=course_ids,
         library_key=library_key,
         source_type=source_type
     )
--- cms/djangoapps/course_to_library_import/models.py
+++ cms/djangoapps/course_to_library_import/models.py
@@ -1,19 +1,20 @@
 import logging
+from django.contrib.auth import get_user_model
 from django.db import models
 
-from opaque_keys.edx.keys import CourseKey
+from opaque_keys.edx.django.models import CourseKeyField, UsageKeyField
+from opaque_keys.edx.keys import CourseKey, UsageKey
 from opaque_keys.edx.locator import LibraryLocatorV2
 from openedx.core.djangoapps.content_libraries import api as contentlib_api
 
+from openedx_learning.apps.authoring.components.models import ComponentVersion
+
+from xblock.core import XBlock
+from xmodule.modulestore.django import modulestore
+from model_utils.models import TimeStampedModel
 
 log = logging.getLogger(__name__)
-
-
-
-from opaque_keys.edx.keys import CourseKey
-from xblock.core import XBlock
-
-from xmodule.modulestore.django import modulestore
+User = get_user_model()
 
 
 def get_course_structure(course_id):
@@ -27,43 +28,61 @@ def get_block_descendants(block: XBlock) -> dict:
     """
     Provides block descendants.
     """
-    descendants = []
-
     return {
         str(child.location): get_block_descendants(child)
         for child in block.get_children()
     }
 
 
+class CourseToLibraryImport(TimeStampedModel):
+    """
+    Represents a course import into a content library.
+    """
 
+    # TODO: move this to data.py as a choice class
+    PENDING = 'PENDING'
+    READY = 'READY'
+    IMPORTED = 'IMPORTED'
+    ERROR = 'ERROR'
 
-class CourseToLibraryImport(models.Model):
+    IMPORT_STATUSES = (
+        (PENDING, 'Pending'),
+        (READY, 'Ready'),
+        (IMPORTED, 'Imported'),
+        (ERROR, 'Error'),
+    )
+
     date = models.DateField(auto_now_add=True)
-    state = models.CharField(max_length=100)
-    course_id = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)  # TODO: delete this field
+    status = models.CharField(max_length=100, choices=IMPORT_STATUSES, default=PENDING)
+    course_ids = models.TextField(
+        blank=False,
+        help_text="Whitespace-separated list of course keys for which to compute grades."
+    )
     library_key = models.CharField(max_length=100)
     source_type = models.CharField(max_length=30)
     metadata = models.JSONField(default=dict, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
-    def save(self, *args, **kwargs):
-        metadata = get_course_structure(self.course_id)
-        self.metadata = metadata
-        super().save(*args, **kwargs)
+    def __str__(self):
+        return f"{self.course_ids} - {self.library_key}"
 
-        openedx_client = contentlib_api.EdxModulestoreImportClient(
-            library_key=LibraryLocatorV2.from_string(self.library_key),
-        )
+    class Meta:
+        verbose_name = 'Course to Library Import'
+        verbose_name_plural = 'Course to Library Imports'
 
-        failed_blocks = []
 
-        def _on_progress(block_key, block_num, block_count, exception=None):
-            if exception:
-                log.error('Failed to import block: %s', block_key, exc_info=exception)
-                failed_blocks.append(block_key)
-            else:
-                log.info('Block imported: %s', block_key)
 
-        openedx_client.import_blocks_from_course(
-            CourseKey.from_string(self.course_id),
-            _on_progress
-        )
+class ComponentVersionImport(TimeStampedModel):
+    """
+    Represents a component version that has been imported into a content library.
+
+
+    """
+
+    component_version = models.OneToOneField(ComponentVersion, on_delete=models.CASCADE)
+    source_usage_key = UsageKeyField(max_length=255)
+    library_import = models.ForeignKey(CourseToLibraryImport, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.component_version} - {self.source_usage_key}"
