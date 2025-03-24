706888cc344a137ccc9a1c245d6adf8e3329bb2d
Іван Нєдєльніцев <ivan.niedielnitsev@raccoongang.com>
2025-03-24 17:17:51 +0200

    feat: REST API to get and create imports

--- cms/djangoapps/course_to_library_import/views/v0/urls.py
+++ cms/djangoapps/course_to_library_import/views/v0/urls.py
@@ -4,9 +4,11 @@ Course to Library Import API v0 URLs.
 
 from django.urls import path
 
-from .views import ImportBlocksView
+from .views import CreateCourseToLibraryImportView, ImportBlocksView, GetCourseStructureToLibraryImportView
 
 app_name = 'v0'
 urlpatterns = [
     path('import_blocks/', ImportBlocksView.as_view(), name='import_blocks'),
+    path('create_import/<str:lib_key_str>/', CreateCourseToLibraryImportView.as_view(), name='create_import'),
+    path('get_import/<uuid:course_to_lib_uuid>/', GetCourseStructureToLibraryImportView.as_view(), name='get_import'),
 ]
