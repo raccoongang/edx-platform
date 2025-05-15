23bb9f6ac079657fee238c78a80252edea02829d
Andrii <andrii.hantkovskyi@raccoongang.com>
2025-05-15 17:03:20 +0300

    refactor: redesign waffleflag & add new MFE toggle to FEATURES

---
+++ lms/djangoapps/branding/toggles.py
@@ -0,0 +1,35 @@
+"""
+Configuration for features of Branding
+"""
+from django.conf import settings
+from edx_toggles.toggles import WaffleFlag
+
+from openedx.core.djangoapps.site_configuration import helpers as configuration_helpers
+
+# Namespace for Waffle flags related to branding
+WAFFLE_FLAG_NAMESPACE = "legacy_brandind"
+
+
+def catalog_mfe_enabled():
+    """
+    Determine if Catalog MFE is enabled, replacing student_dashboard
+    """
+    return configuration_helpers.get_value(
+        'ENABLE_CATALOG_MICROFRONTEND', settings.FEATURES.get('ENABLE_CATALOG_MICROFRONTEND')
+    )
+
+
+# .. toggle_name: legacy_branding.catalog
+# .. toggle_implementation: WaffleFlag
+# .. toggle_default: False
+# .. toggle_description: Fall back to the old LMS catalog page.
+# .. toggle_creation_date: 2025-05-15
+# .. toggle_target_removal_date: None
+LEGACY_CATALOG = WaffleFlag(f'{WAFFLE_FLAG_NAMESPACE}.catalog', __name__)
+
+
+def use_new_catalog_page():
+    """
+    Returns a boolean if new catalog page should be used.
+    """
+    return not LEGACY_CATALOG.is_enabled()
