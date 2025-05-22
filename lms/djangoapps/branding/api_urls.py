8d16bf9fcae20733774b82d70ac236a1ead435b1
Andrii <andrii.hantkovskyi@raccoongang.com>
2025-05-22 15:36:27 +0300

    feat: [AXM-2078] implement waffle flag confi api for new MFEs

--- lms/djangoapps/branding/api_urls.py
+++ lms/djangoapps/branding/api_urls.py
@@ -5,8 +5,9 @@ Branding API endpoint urls.
 
 from django.urls import path
 
-from lms.djangoapps.branding.views import footer
+from lms.djangoapps.branding.views import footer, waffle_flags
 
 urlpatterns = [
     path('footer', footer, name="branding_footer"),
+    path('waffle-flags', waffle_flags, name="branding_waffle_flags"),
 ]
