7237479b173f604f24f71a7d250de2ed676a9c36
Serhii Nanai <serhii.nanai@raccoongang.com>
2025-07-03 21:15:23 +0300

    chore: restore LMSFrontendParamsView and tests

--- lms/djangoapps/branding/api_urls.py
+++ lms/djangoapps/branding/api_urls.py
@@ -3,9 +3,9 @@ Branding API endpoint urls.
 """
 
 
-from django.urls import path, re_path
+from django.urls import path
 
-from lms.djangoapps.branding.views import footer, LMSFrontendParamsView, WaffleFlagsView
+from lms.djangoapps.branding.views import footer, LMSFrontendParamsView
 from openedx.core.constants import COURSE_ID_PATTERN
 
 urlpatterns = [
